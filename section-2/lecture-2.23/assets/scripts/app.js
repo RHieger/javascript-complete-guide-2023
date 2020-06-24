0817cd2a524f0e5c130d0f7bd897d2c000e3ce0f
Robert Hieger <rhieger@gmail.com>
2020-06-24 01:32:02 -0400

    Complete lecture-2.23: Refactor to display calculation description

--- section-2/lecture-2.23/assets/scripts/app.js
+++ section-2/lecture-2.23/assets/scripts/app.js
@@ -1,6 +1,9 @@
 let currentResult = 0;
 
 // Expected return value: 14
-currentResult = (currentResult + 10) * 3 /2 -1;
+currentResult = (currentResult + 10) * 3 / 2 - 1;
 
-outputResult(currentResult, '');
+// Expected value is stringified version of above calculation.
+let calculationDescription = '(' + currentResult + ' + 10) * 3 / 2 - 1';
+
+outputResult(currentResult, calculationDescription);
