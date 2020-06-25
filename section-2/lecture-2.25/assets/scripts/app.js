80140a4e4070c6ee6dad6d3df8024be797dfc0a7
Robert Hieger <rhieger@gmail.com>
2020-06-24 21:03:36 -0400

    Complete lecture-2.25: Refactor string with template literal

--- section-2/lecture-2.25/assets/scripts/app.js
+++ section-2/lecture-2.25/assets/scripts/app.js
@@ -5,6 +5,6 @@ let currentResult = 0;
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 // Expected value is stringified version of above calculation. Due
-let calculationDescription = '(' + defaultResult + ' + 10) * 3 / 2 - 1';
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
 
 outputResult(currentResult, calculationDescription);
