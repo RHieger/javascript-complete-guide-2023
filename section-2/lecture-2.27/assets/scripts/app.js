20458e0a2adbf510c3d9a41edbf3dd8819c27fc9
Robert Hieger <rhieger@gmail.com>
2020-06-28 16:17:36 -0400

    Complete lecture-2.27: implement add() function

--- section-2/lecture-2.27/assets/scripts/app.js
+++ section-2/lecture-2.27/assets/scripts/app.js
@@ -1,6 +1,14 @@
 const defaultResult = 0;
 let currentResult = 0;
 
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert(`${num1} + ${num2} = ${result}`);
+}
+
+add(1, 2);
+add(5, 5);
+
 // Expected return value: 14
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
