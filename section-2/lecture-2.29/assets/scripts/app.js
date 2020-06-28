048436769d12408a1e9f7f31e646b3be5d347db5
Robert Hieger <rhieger@gmail.com>
2020-06-28 18:53:35 -0400

    Complete lecture-2.29: Refactor add() as pure function

--- section-2/lecture-2.29/assets/scripts/app.js
+++ section-2/lecture-2.29/assets/scripts/app.js
@@ -3,16 +3,13 @@ let currentResult = 0;
 
 function add(num1, num2) {
   const result = num1 + num2;
-  alert(`${num1} + ${num2} = ${result}`);
+  return result;
 }
 
-add(1, 2);
-add(5, 5);
+// Expected return value: 3
+currentResult = add(1, 2);
 
-// Expected return value: 14
-currentResult = (currentResult + 10) * 3 / 2 - 1;
-
-// Expected value is stringified version of above calculation. Due
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+// Expected value is stringified version of above calculation.
+let calculationDescription = `( ${defaultResult} + (1 + 2) )`;
 
 outputResult(currentResult, calculationDescription);
