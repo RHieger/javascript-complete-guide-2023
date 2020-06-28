9106039057e93c05d56fe9437179364a16cbcc8e
Robert Hieger <rhieger@gmail.com>
2020-06-28 12:41:38 -0400

    Initialize lecture-2.26

---
+++ section-2/lecture-2.26/assets/scripts/app.js
@@ -0,0 +1,10 @@
+const defaultResult = 0;
+let currentResult = 0;
+
+// Expected return value: 14
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+// Expected value is stringified version of above calculation. Due
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
