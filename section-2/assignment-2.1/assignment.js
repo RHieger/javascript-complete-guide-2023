03d7291fedd94c9b2dab74fd51d1c927bc762d28
Robert Hieger <rhieger@gmail.com>
2020-06-28 11:56:49 -0400

    Implment Step 1 of Assignment 2.1

---
+++ section-2/assignment-2.1/assignment.js
@@ -0,0 +1,14 @@
+/**
+ * STEP 1: Create two variables, one for fictional user input, and
+ * another to hold a result of a fictitious calculation.
+ * 
+ * @constant { integer } input -- fictious user entry
+ * @var { integer } calculation -- result of calculation
+ * 
+ */
+
+ const USER_INPUT = 29;
+
+ let calcResult;
+
+ 
