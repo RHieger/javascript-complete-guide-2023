6711d7b534aabce2f4f2e97032d00251693990f9
Robert Hieger <rhieger@gmail.com>
2020-06-30 20:52:32 -0400

    Completed Task 1 for assignment-2.2

--- section-2/assignment-2.2/assignment.js
+++ section-2/assignment-2.2/assignment.js
@@ -1,1 +1,20 @@
+// Gets DOM element for Task 3.
 const task3Element = document.getElementById('task-3');
+
+/**
+ * Task 1:
+ * 
+ * @function greet() -- generic message
+ * 
+ * @function myName(name) -- introduction
+ * 
+ */
+
+ const greet = () => {
+  alert('Hi, JavaScript can be fun! 🎹 🌈');
+ };
+
+ const myName = (name) => {
+  alert(`Hi, my name is ${name}!`);
+ };
+ 
