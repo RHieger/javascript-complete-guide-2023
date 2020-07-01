5c9cfe156c1d4a1938a54bb8eb8a5de9f0b8754b
Robert Hieger <rhieger@gmail.com>
2020-07-01 19:16:15 -0400

    Complete lecture-2.36: Refactor add() to parse number correctly

--- section-2/lecture-2.36/assets/scripts/app.js
+++ section-2/lecture-2.36/assets/scripts/app.js
@@ -2,14 +2,8 @@ const defaultResult = 0;
 let currentResult = 0;
 
 function add(num1, num2) {
-  const result = num1 + num2;
-  return result;
+  currentResult += parseInt(userInput.value);
+  outputResult(currentResult, '');
 }
 
-// Expected return value: 3
-currentResult = add(1, 2);
-
-// Expected value is stringified version of above calculation.
-let calculationDescription = `( ${defaultResult} + (1 + 2) )`;
-
-outputResult(currentResult, calculationDescription);
+addBtn.addEventListener('click', add);
