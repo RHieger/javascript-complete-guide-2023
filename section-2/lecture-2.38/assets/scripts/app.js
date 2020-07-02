a7de1a1298b5856abff1c1991982bffa5674bb91
Robert Hieger <rhieger@gmail.com>
2020-07-01 20:17:03 -0400

    Complete lecture-2.36: export user input to separate function

--- section-2/lecture-2.38/assets/scripts/app.js
+++ section-2/lecture-2.38/assets/scripts/app.js
@@ -1,8 +1,12 @@
 const defaultResult = 0;
 let currentResult = 0;
 
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
 function add(num1, num2) {
-  const enteredNumber = parsInt(userInput.value);
+  const enteredNumber = getUserNumberInput();
   const calcDescription = `${currentResult} + ${enteredNumber}`
   currentResult += enteredNumber;
   outputResult(currentResult, calcDescription);
