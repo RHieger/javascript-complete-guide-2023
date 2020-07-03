e9c7a92e2a6e29059c3937be222a03f91e84f6ce
Robert Hieger <rhieger@gmail.com>
2020-07-03 15:38:47 -0400

    Migrated code from lecture-2.43 to lecture-2.44

---
+++ section-2/lecture-2.44/assets/scripts/app.js
@@ -0,0 +1,54 @@
+const defaultResult = 0;
+let currentResult = 0;
+let logEntries = [];
+
+// Gets user input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log.
+function createAndWriteOutput(
+  operator,
+  resultBeforeCalc,
+  operand  // NOTE: instructor named this calcNumber
+) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${operand}`;
+  outputResult(currentResult, calcDescription);  // From vendor.js
+}
+
+function add(num1, num2) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  logEntries.push(enteredNumber);
+  console.log(logEntries[0]);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
