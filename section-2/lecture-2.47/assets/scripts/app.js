1aead83fccbdaae6bdb60b5c354981a701c1a7b4
Robert Hieger <rhieger@gmail.com>
2020-07-05 11:05:12 -0400

    Migrate code from lecture-2.44 to lecture-2.47

---
+++ section-2/lecture-2.47/assets/scripts/app.js
@@ -0,0 +1,60 @@
+const defaultResult = 0;
+let currentResult = 0;
+let logEntries = [];
+
+// Gets user input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log.
+function createAndWriteOutput(
+  operator,
+  resultBeforeCalc,
+  operand  // NOTE: instructor named this calcNumber
+) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${operand}`;
+  outputResult(currentResult, calcDescription);  // From vendor.js
+}
+
+function add(num1, num2) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber);
+  const logEntry = {
+    operation: 'ADD',
+    previousResult: initialResult,
+    operand: enteredNumber,
+    sum: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+
+addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
