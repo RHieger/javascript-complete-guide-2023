075f8094ea1f550a20e2b8110f31bb7aa082f5e0
Robert Hieger <rhieger@gmail.com>
2020-07-31 16:06:03 -0400

    Complete Lecture 4.87: Refactor healPlayerHandler() to prevent healing beyond 100%

--- section-4/lecture-4.87/assets/scripts/app.js
+++ section-4/lecture-4.87/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 10;   // strength of attack
 const STRONG_ATTACK_VALUE = 17; // strength of strong attack
 const MONSTER_ATTACK_VALUE = 14; // strength of monster attack
+const HEAL_VALUE = 20;  // extent to which player is healed
 
 let chosenMaxLife = 100;  // hard-coded health points for player
 let currentMonsterHealth = chosenMaxLife;
@@ -8,15 +9,7 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode)  {
-  let maxDamage;  // value of attack strength
-  if (mode === "ATTACK") {
-    maxDamage = ATTACK_VALUE;
-  }  else if (mode === 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-  const damage = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= damage;
+function endRound()  {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
   if (
@@ -37,6 +30,18 @@ function attackMonster(mode)  {
   }
 }
 
+function attackMonster(mode)  {
+  let maxDamage;  // value of attack strength
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  }  else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
+}
+
 function attackHandler() {
   attackMonster('ATTACK');
 }
@@ -45,5 +50,19 @@ function strongAttackHandler()  {
   attackHandler('STRONG_ATTACK');
 }
 
+function healPlayerHandler()  {
+  let healValue;
+  if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE)  {
+    alert('You can\'t heal to more than your max initial health.');
+    healValue = chosenMaxLife - currentPlayerHealth;
+  } else {
+    healValue = HEAL_VALUE;
+  }
+  increasePlayerHealth(healValue); // call from vendor.js
+  currentPlayerHealth += healValue;  // update to reflect healing
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
