ebdfe0e0fcbfa147a5bb0fdb8a3dd45c7b12527f
Robert Hieger <rhieger@gmail.com>
2020-07-31 14:36:14 -0400

    Complete lecture-4.86: Refactor attackHandler() to handle ATTACK and STRONG_ATTACK

--- section-4/lecture-4.86/assets/scripts/app.js
+++ section-4/lecture-4.86/assets/scripts/app.js
@@ -8,8 +8,14 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode)  {
+  let maxDamage;  // value of attack strength
+  if (mode === "ATTACK") {
+    maxDamage = ATTACK_VALUE;
+  }  else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
   currentMonsterHealth -= damage;
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
@@ -31,27 +37,12 @@ function attackHandler() {
   }
 }
 
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
 function strongAttackHandler()  {
-  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-  if (
-      currentMonsterHealth <= 0 &&
-      currentPlayerHealth > 0
-    ) {
-    alert('You Won!');
-  }  else if (
-      currentPlayerHealth <= 0 &&
-      currentMonsterHealth > 0
-    ) {
-    alert('You Lost!');
-  }  else if (
-    currentPlayerHealth <=0 &&
-    currentMonsterHealth <= 0
-  )  {
-    alert('You have a draw!');
-  }
+  attackHandler('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
