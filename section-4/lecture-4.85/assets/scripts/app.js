b9de94c57ec415c4f87705c47473f941636a9f5c
Robert Hieger <rhieger@gmail.com>
2020-07-31 01:21:21 -0400

    Complete lecture-4.85: Add conditional logic for attacks

--- section-4/lecture-4.85/assets/scripts/app.js
+++ section-4/lecture-4.85/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;   // strength of attack
+const MONSTER_ATTACK_VALUE = 14; // strength of monster attack
 
 let chosenMaxLife = 100;  // hard-coded health points for player
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,24 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (
+      currentMonsterHealth <= 0 &&
+      currentPlayerHealth > 0
+    ) {
+    alert('You Won!');
+  }  else if (
+      currentPlayerHealth <= 0 &&
+      currentMonsterHealth > 0
+    ) {
+    alert('You Lost!');
+  }  else if (
+    currentPlayerHealth <=0 &&
+    currentMonsterHealth <= 0
+  )  {
+    alert('You have a draw!');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
