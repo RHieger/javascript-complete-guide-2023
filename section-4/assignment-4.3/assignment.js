6cefd4c16854bbfadf5f72e0daa646c133da7a68
Robert Hieger <rhieger@gmail.com>
2020-08-06 21:04:39 -0400

    Complete Step 2: Loop through array of numbers two different ways

--- section-4/assignment-4.3/assignment.js
+++ section-4/assignment-4.3/assignment.js
@@ -1,6 +1,20 @@
 const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
 
 const task1Btn = document.getElementById('task1');
+const task2Btn = document.getElementById('task2');
+
+const numbers = [
+  25,
+  71,
+  103,
+  26,
+  94,
+  1000,
+  157,
+  356,
+  -7,
+  58
+];
 
 // STEP 1: Above is a variable that holds a random number between 0 and 1.
 // Write code that shows an alert (with any message) when that number is
@@ -18,4 +32,23 @@ This is not greater than 0.7.`);
   }
 }
 
+function arrayLoop() {
+  console.log(`PASS #1 (Original Order):\n-----------------------\n`);
+  let i = 0;
+  numbers.forEach(element => {
+    console.log(`Element #${i} => ${element}`);
+    i++;
+  });
+  console.log(`PASS #2 (Descending Order Sort):\n
+-----------------------------\n`);
+  let j = 0;
+  const reverseSortNums = numbers.sort( (a, b) => b-a );
+  reverseSortNums.forEach(element => {
+    console.log(`Element #${j} => ${element}`);
+  });
+} // STEP 2: Create an array of numbers (any numbers of your choice) and
+// loop through the array in two different ways - outputting the numbers
+// inside of the loop.
+
 task1Btn.addEventListener('click', isGreaterThanPoint7);
+task2Btn.addEventListener('click', arrayLoop);
