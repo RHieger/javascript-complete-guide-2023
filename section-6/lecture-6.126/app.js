57d36532147313621e8ccc751f52e348e028e4c3
Robert Hieger <rhieger@gmail.com>
2020-08-09 18:09:45 -0400

    Complete Lecture 6.126: Illustrate difference between function and method

--- section-6/lecture-6.126/app.js
+++ section-6/lecture-6.126/app.js
@@ -1,1 +1,24 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+  console.log('Game is starting...');
+}
+
+// Object stores both property and a function
+// as appears below in person() object
+
+const person = {
+  name: 'Bobby',
+  greet: function() {
+    console.log(`Hi, ${this.name}!`);
+  }
+}
+
+// Here the greet function that is a member of
+// the person() object is called. This function
+// is known as a method because it occurs within
+// an object. 
+person.greet();
+
+// addEventListener is a method of the document() Object.
+startGameBtn.addEventListener('click', startGame);
