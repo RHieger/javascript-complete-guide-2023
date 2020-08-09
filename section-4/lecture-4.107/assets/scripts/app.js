fe79dbc23bb3f8beeb6dd8c5d5fcf7aaee64bb58
Robert Hieger <rhieger@gmail.com>
2020-08-08 20:30:04 -0400

    Complete Lecture 4.107: Add error handling that throws custom error

--- section-4/lecture-4.107/assets/scripts/app.js
+++ section-4/lecture-4.107/assets/scripts/app.js
@@ -12,15 +12,21 @@ const LOG_EVENT_MONSTER_ATTACK = 'MONSTER_ATTACK'; // Monster Attack
 const LOG_EVENT_PLAYER_HEAL = 'PLAYER_HEAL'; // Player Heal Event
 const LOG_EVENT_GAME_OVER = 'GAME_OVER'; // Game Over Event
 
-const enteredValue = prompt('Maximum life for you and the monster.', '100');
+function getMaxLifeValue () {
+  const enteredValue = prompt('Maximum life for you and the monster.', '100');
 
-let chosenMaxLife = parseInt(enteredValue);
+  const parsedValue = parseInt(enteredValue);
+  if (isNaN(parsedValue) || parsedValue <= 0) {
+    throw { message: 'Invalid user input, not a number!' };
+  }
+  return parsedValue;
+}
+
+let chosenMaxLife = getMaxLifeValue();
+
 let battleLog = [];
 let lastLoggedEntry;
 
-if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
-  chosenMaxLife = 100;
-}
 
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
