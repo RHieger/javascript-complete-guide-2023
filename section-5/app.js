f51eabc18126ea090075fde7b9f6f9bb431f9b0b
Robert Hieger <rhieger@gmail.com>
2020-08-09 16:25:34 -0400

    Demonstrate local vs. global scope: lecture-5.113

--- section-5/app.js
+++ section-5/app.js
@@ -1,7 +1,15 @@
 let name = 'Bobby';
 
 function greet() {
-  console.log(name);
+  let age = 58;
+  // shadowed variable. Will only be used by console.log()
+  // inside the greet() function--local scope
+  let name = 'Joanie';
+  console.log(name, age);
 }
+
+// console.log(name, age); // will not work because age is local scope
+
+console.log(name);  // This will display Bobby, name at global scope
 
 greet();
