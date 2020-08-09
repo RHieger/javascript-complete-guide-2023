07e70a7e6a1e1c29f9e5af91b34a242878933f53
Robert Hieger <rhieger@gmail.com>
2020-08-09 19:11:57 -0400

    Complete Lecture 7.147: Demonstrate extraction of browser properties

---
+++ section-7/app.js
@@ -0,0 +1,8 @@
+// Maximilliann Schwarzmüller did not even use this
+// file in his demonstration, but rather only the
+// JavaScript console window. I will demonstrate the
+// extraction of a simple piece of information from
+// the top level of the DOM--Window.
+
+console.dir(window.navigator.platform);
+console.dir(window.navigator.plugins);
