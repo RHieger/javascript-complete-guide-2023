b8e2a31b98998931304a182cabaae7ba09f0998c
Robert Hieger <rhieger@gmail.com>
2020-08-10 19:50:01 -0400

    Complete lecture-1.185: Convert node list to true array

--- section-8/lecture-8.185/app.js
+++ section-8/lecture-8.185/app.js
@@ -9,15 +9,26 @@ const stringArray = new Array('Hi!', 'Welcome!');
 // return an empty array with the number of elemnts
 // specified in that one argument. Slower performance
 // than prevalent declaration type.
-const yetMoreNumbers = new Array(5);
+// const yetMoreNumbers = new Array(5);
+
+// More useful means of declaring an array.
+// This will take an array-like object and
+// split it into individual elements.
+const yetMoreNumbers = Array.from('Hello!');
 
 // Another way of declaring an array. Very rarely
 // used, and slower performance than prevalent
 // declaration type.
-const stillMoreNumbers = Array.of(4, 5, 6);
+// const stillMoreNumbers = Array.of(4, 5, 6);
+
+// Create a node list from nodes in the DOM:
+const listItems = document.querySelectorAll('li');
+
+const arrayListItems = Array.from(listItems);
 
 console.log(numbers);
 console.log(moreNumbers);  // will log empty array
 console.log(stringArray);
 console.log(yetMoreNumbers);
-console.log(stillMoreNumbers);
+// console.log(stillMoreNumbers);
+console.log(arrayListItems);
