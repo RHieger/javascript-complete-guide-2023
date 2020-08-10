2bfdbd56c88d5d6a03a74458b18fc438350266bb
Robert Hieger <rhieger@gmail.com>
2020-08-10 00:30:03 -0400

    Explore 3 common ways to declare an array

--- section-8/lecture-8.185/app.js
+++ section-8/lecture-8.185/app.js
@@ -1,3 +1,23 @@
 const numbers = [1, 2, 3];  // most common way of creating array
 
+// Less common way to create array using constructor
+const moreNumbers = new Array();
+
+const stringArray = new Array('Hi!', 'Welcome!');
+
+// Just one argument to the Array constructor will
+// return an empty array with the number of elemnts
+// specified in that one argument. Slower performance
+// than prevalent declaration type.
+const yetMoreNumbers = new Array(5);
+
+// Another way of declaring an array. Very rarely
+// used, and slower performance than prevalent
+// declaration type.
+const stillMoreNumbers = Array.of(4, 5, 6);
+
 console.log(numbers);
+console.log(moreNumbers);  // will log empty array
+console.log(stringArray);
+console.log(yetMoreNumbers);
+console.log(stillMoreNumbers);
