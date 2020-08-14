971ec7a89894e9c9afbaef01dd8326c4e7538466
Robert Hieger <rhieger@gmail.com>
2020-08-14 13:05:06 -0400

    Add second instance of value 1.5 to storedValues[] to demonstrate lastIndexOf()

--- section-8/lecture-8.191/app.js
+++ section-8/lecture-8.191/app.js
@@ -1,4 +1,4 @@
-const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
 
 console.log('testResults[] => ', testResults);
 
@@ -6,3 +6,13 @@ console.log('Creating copy of testResults[] with 2 new elements at the end...]')
 const storedResults = testResults.concat([3.99, 2]);
 console.log('testResults[] => ', testResults);
 console.log('storedResults[] => ', storedResults);
+
+// NOTE: The indexOf() method always returns the first index only
+// that matches the search parameter if there is more than one
+// instance of the same value.
+console.log(`Index of value 1.5 => ${storedResults.indexOf(1.5)}`);
+
+// NOTE: The lastIndexOf() method will find the last index
+// of its search parameter, searching from the end of the
+// array backward until it finds a match.
+console.log(`Index last value instance of 1.5 => ${storedResults.lastIndexOf(1.5)}`);
