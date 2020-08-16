fb56324c316361a06cb2e8604b2898969762312e
Robert Hieger <rhieger@gmail.com>
2020-08-16 11:46:01 -0400

    Remove superfluous code

--- section-8/lecture-8.196/app.js
+++ section-8/lecture-8.196/app.js
@@ -1,13 +1,3 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
-const taxAdjustedPrices = prices.map( (price, index, prices) => {
-  const adjustedPrices = {
-    index: index,
-    taxAdjustedPrice: price * (1 + tax)
-  };
-  return adjustedPrices;
-});
-
-console.log('Pre-Tax Prices => ', prices);
-console.log('Tax-Adjusted Prices => ', taxAdjustedPrices);
