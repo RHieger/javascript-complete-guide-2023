11756c9aa31ff115108f855d9bc15fbfe02f9740
Robert Hieger <rhieger@gmail.com>
2020-08-16 13:51:44 -0400

    Sort prices using sort() with compare function

--- section-8/lecture-8.196/app.js
+++ section-8/lecture-8.196/app.js
@@ -1,3 +1,16 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
 
+console.log('prices => ', prices);
+
+const sortedPrices = prices.sort( (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else if (a < b) {
+    return -1;
+  }
+});
+
+console.log('sortedPrices => ', sortedPrices);
