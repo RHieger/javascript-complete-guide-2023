58459ff9ee72c1c7ee9e9f6ff46130e213d28799
Robert Hieger <rhieger@gmail.com>
2020-08-16 15:29:47 -0400

    Complete Lecture 8.198: Refactor filter() to use arrow function

--- section-8/lecture-8.198/app.js
+++ section-8/lecture-8.198/app.js
@@ -2,9 +2,7 @@ const prices = [10.99, 5.99, 3.99, 6.59];
 
 const tax = 0.19;
 
-const filteredArray = prices.filter( (price, index, prices) => {
-  return price > 6;
-});
+const filteredArray = prices.filter(price  => price > 6);
 
 // NOTE: Original array remains untouched.
 console.log('prices: ', prices);
