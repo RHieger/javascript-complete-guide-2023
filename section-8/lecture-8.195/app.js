a0477e8a5843358f8c5d92b4761a635f8f0af8b7
Robert Hieger <rhieger@gmail.com>
2020-08-16 11:28:10 -0400

    Complete Lecture 8.195: Output Tax Adjusted Prices Using map()

--- section-8/lecture-8.195/app.js
+++ section-8/lecture-8.195/app.js
@@ -1,25 +1,13 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
-// THE WAY ALREADY EXPLORED:
-
-console.log('prices => ', prices);
-for (const price of prices) {
-  taxAdjustedPrices.push( price * (1 + tax) );
-}
-
-console.log('Tax Adjusted Prices => ', taxAdjustedPrices);
-
-// ANOTHER WAY OF DOING THE ABOVE:
-
-console.log('Outputting Tax Adjusted Prices Using forEach():');
-
-prices.forEach( (price, index, prices) => {
-  const adjustedPrice = {
+const taxAdjustedPrices = prices.map( (price, index, prices) => {
+  const adjustedPrices = {
     index: index,
     taxAdjustedPrice: price * (1 + tax)
   };
-  taxAdjustedPrices.push(adjustedPrice);
-  console.log(adjustedPrice);
+  return adjustedPrices;
 });
+
+console.log('Pre-Tax Prices => ', prices);
+console.log('Tax-Adjusted Prices => ', taxAdjustedPrices);
