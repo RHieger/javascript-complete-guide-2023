bb1cb2c634fb018319db95f1fd2c67a8ffcb63ad
Robert Hieger <rhieger@gmail.com>
2020-08-18 21:01:40 -0400

    Use spread operator to make copy of nameFragments[]

--- section-8/lecture-8.202/app.js
+++ section-8/lecture-8.202/app.js
@@ -1,1 +1,17 @@
 const nameFragments = ['Robert', 'Hieger'];
+
+// Spread operator copies contents of nameFragments
+// into a new array.
+const copiedNameFragments = [...nameFragments];
+
+console.log('nameFragments => ', nameFragments);
+console.log('copiedNameFragments => ', copiedNameFragments);
+
+// Demonstrate that nameFragments and copiedNameFragments are
+// actually two distinct objects with their own unique memory
+// addresses. Changes to one do not affect the other.
+
+nameFragments.push('Mr.');
+
+console.log('Updated nameFragments[] => ', nameFragments);
+console.log('copiedNameFragments[] => ', copiedNameFragments);
