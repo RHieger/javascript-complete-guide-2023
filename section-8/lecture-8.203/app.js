ce716b2fe612ecf599a7a42bfac9ba9f7f54c772
Robert Hieger <rhieger@gmail.com>
2020-08-19 21:10:36 -0400

    Use destructuring to print nameData[] in contiguous string

--- section-8/lecture-8.203/app.js
+++ section-8/lecture-8.203/app.js
@@ -1,2 +1,13 @@
-const nameFragments = ['Robert', 'Hieger'];
+const nameData = ['Robert', 'Hieger'];
 
+console.log('nameData =>');
+console.table(nameData);
+
+// Demonstrate destructing nameData
+
+// NOTE: the constant names in the array bracket
+// are assigned one after the other to each
+// successive element in the nameData array.
+const [ firstName, lastName ] = nameData;
+
+console.log('Destructured nameData[] =>\n', firstName, lastName);
