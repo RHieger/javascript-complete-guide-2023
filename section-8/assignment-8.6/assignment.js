50c20737718191ffa98270538d309eb03bf923f6
Robert Hieger <rhieger@gmail.com>
2020-08-22 14:27:59 -0400

    Got all ordered list items for use in event listeners

---
+++ section-8/assignment-8.6/assignment.js
@@ -0,0 +1,4 @@
+const task1 = document.getElementById('task1');
+const task2 = document.getElementById('task2');
+const task3 = document.getElementById('task3');
+const task1 = document.getElementById('task4');
