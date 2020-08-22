a3106f4655a210ad2c92d37b3cbf3b092f050145
Robert Hieger <rhieger@gmail.com>
2020-08-22 15:46:53 -0400

    Implemented Task 1: Assignment 8.6

--- section-8/assignment-8.6/assignment.js
+++ section-8/assignment-8.6/assignment.js
@@ -1,4 +1,39 @@
 const task1 = document.getElementById('task1');
 const task2 = document.getElementById('task2');
 const task3 = document.getElementById('task3');
-const task1 = document.getElementById('task4');
+const task4 = document.getElementById('task4');
+
+const numbers = [1, 5, 7, 25, 36, 227, 58, 39, 22, -37];
+
+const task1Handler = () => {
+  console.log('numbers[] =>');
+  console.table(numbers);
+  
+  const filteredSet = numbers.filter( number => number > 6 );
+  console.log('numbers[] filtered for numbers > 5 =>');
+  console.table(filteredSet);
+  
+  console.log('Map numbers[] to objects =>');
+  const mappedNumbers = numbers.map( (value) => {
+    const item = { number: value };
+    return item;
+  });
+  console.log(mappedNumbers);
+
+  console.log('Return reduced numbers[] =>');
+  const product = numbers.reduce(
+    (
+      previousValue,
+      currentValue,
+      currentIndex,
+      numbers
+    ) => {
+      return previousValue *= currentValue;
+    }, 1);
+  console.log('product of numbers[] =>');
+  console.log(product);
+}
+
+  // EVENT LISTENERS:
+
+task1.addEventListener('click', task1Handler);
