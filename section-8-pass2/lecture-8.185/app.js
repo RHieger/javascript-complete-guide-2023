e47d0f913720225c6d0933dd5948debb8eb21aea
Robert Hieger <rhieger@gmail.com>
2020-08-23 11:40:56 -0400

    Create and log to console array using Array() constructor and omitting new keyword

--- section-8-pass2/lecture-8.185/app.js
+++ section-8-pass2/lecture-8.185/app.js
@@ -12,3 +12,11 @@ const moreNumbers = new Array(4, 5);
 
 console.log('moreNumbers[] array created using Array() constructor =>');
 console.table(moreNumbers);
+
+// Create array with Array() Constructor
+// Omitting new keyword: Method #3
+const andMoreNumbers = Array(10, 12);
+
+console.log('andMoreNumbers[] created with Array() constructor and' +
+' omitting new keyword =>');
+console.table(andMoreNumbers);
