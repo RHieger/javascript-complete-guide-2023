9e877672b7791df2fd79fd7ea53103a1bfd9482e
Robert Hieger <rhieger@gmail.com>
2020-08-23 11:49:49 -0400

    Create yetMoreNumbers[] using Array.of() and log to console

--- section-8-pass2/lecture-8.185/app.js
+++ section-8-pass2/lecture-8.185/app.js
@@ -5,9 +5,10 @@ console.log('numbers[] array created using bracket notation =>');
 console.table(numbers);
 
 // Create an array using Array() constructor: Method #2
-// will initialize empty array with length of 5
 
+// will initialize empty array with length of 5
 // const moreNumbers = new Array(5);
+
 const moreNumbers = new Array(4, 5);
 
 console.log('moreNumbers[] array created using Array() constructor =>');
@@ -20,3 +21,9 @@ const andMoreNumbers = Array(10, 12);
 console.log('andMoreNumbers[] created with Array() constructor and' +
 ' omitting new keyword =>');
 console.table(andMoreNumbers);
+
+// Create array with Array.of(): Method #4
+const yetMoreNumbers = Array.of(-10, -12);
+
+console.log('yetMoreNumbers[] created using Array.of() =>');
+console.table(yetMoreNumbers);
