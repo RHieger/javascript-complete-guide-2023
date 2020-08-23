35d73bb495c5dd2017649b28ec33a8a133fe023a
Robert Hieger <rhieger@gmail.com>
2020-08-23 11:34:43 -0400

    Initialize moreNumbers[] using Array() constructor and log to console

--- section-8-pass2/lecture-8.185/app.js
+++ section-8-pass2/lecture-8.185/app.js
@@ -3,3 +3,12 @@ const numbers = [1, 2, 3];
 
 console.log('numbers[] array created using bracket notation =>');
 console.table(numbers);
+
+// Create an array using Array() constructor: Method #2
+// will initialize empty array with length of 5
+
+// const moreNumbers = new Array(5);
+const moreNumbers = new Array(4, 5);
+
+console.log('moreNumbers[] array created using Array() constructor =>');
+console.table(moreNumbers);
