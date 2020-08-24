c11d944444eb9bace666a96bcb4b58051e687612
Robert Hieger <rhieger@gmail.com>
2020-08-24 08:56:17 -0400

    Add element to beginning of hobbies[]: unshift()

--- section-8-pass2/lecture-8.187/app.js
+++ section-8-pass2/lecture-8.187/app.js
@@ -1,12 +1,21 @@
 const hobbies = ['Sports', 'Cooking'];
-
 console.log('hobbies =>');
 console.table(hobbies);
 
 // Adding an Element to an Array: Method #1
+// push() -- adds element to end of array
 console.log('Adding an element to hobbies[]...');
 
 hobbies.push('Reading');
+
+console.log('hobbies[] =>');
+console.table(hobbies);
+
+// Adding an Element to an Array: #Method #2
+// unshift() -- adds element to end of array
+console.log('Adding an element to the beginning of hobbies[]...');
+
+hobbies.unshift('Coding');
 
 console.log('hobbies[] =>');
 console.table(hobbies);
