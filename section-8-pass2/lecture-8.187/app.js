c06173cbf6f7d89492c1978ae43ce0fa0878342a
Robert Hieger <rhieger@gmail.com>
2020-08-24 08:44:46 -0400

    Add element to hobbies[] using push()

--- section-8-pass2/lecture-8.187/app.js
+++ section-8-pass2/lecture-8.187/app.js
@@ -2,3 +2,11 @@ const hobbies = ['Sports', 'Cooking'];
 
 console.log('hobbies =>');
 console.table(hobbies);
+
+// Adding an Element to an Array: Method #1
+console.log('Adding an element to hobbies[]...');
+
+hobbies.push('Reading');
+
+console.log('hobbies[] =>');
+console.table(hobbies);
