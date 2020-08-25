98a8282a786da8deb2f755103f54df00c2b3b683
Robert Hieger <rhieger@gmail.com>
2020-08-25 19:50:42 -0400

    Use splice to add new element at index 0 of hobbies[], no deletions

--- section-8-pass2/lecture-8.188/app.js
+++ section-8-pass2/lecture-8.188/app.js
@@ -2,3 +2,9 @@ const hobbies = ['Sports', 'Coding'];
 console.log('hobbies =>');
 console.table(hobbies);
 
+console.log('Using splice() to add an element to hobbies[]...');
+
+// Acts like unshift() but without as much overhead.
+hobbies.splice(0, 0, 'Good Food');
+console.log('hobbies[] =>');
+console.table(hobbies);
