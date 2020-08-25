a4b4660a37ab946549f2f38242a3be65be051bf7
Robert Hieger <rhieger@gmail.com>
2020-08-25 19:58:52 -0400

    Use splice() to add element at 0 index then shift to index 1

--- section-8-pass2/lecture-8.188/app.js
+++ section-8-pass2/lecture-8.188/app.js
@@ -8,3 +8,11 @@ console.log('Using splice() to add an element to hobbies[]...');
 hobbies.splice(0, 0, 'Good Food');
 console.log('hobbies[] =>');
 console.table(hobbies);
+
+// Shift 'Good Food' one index to the right.
+console.log('Shift \'Good Food\' one index to the right using splice()...');
+
+hobbies.splice(0, 1); // first delete item at index 0
+hobbies.splice(1, 0, 'Good Food'); // add back in at index 1
+console.log('hobbies[] =>');
+console.table(hobbies);
