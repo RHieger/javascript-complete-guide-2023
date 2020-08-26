0b803dbad310bc09790b3222233911be19868ba3
Robert Hieger <rhieger@gmail.com>
2020-08-25 21:13:53 -0400

    Use slice() to create copy of a range from testResults[]

--- section-8-pass2/lecture-8.189/app.js
+++ section-8-pass2/lecture-8.189/app.js
@@ -20,3 +20,12 @@ alert('WHY ARE THEY NOT EQUAL?\n-----------------------\n' +
 'Arrays are reference types that store pointers to ' +
 'the data they store. Therefore a true copy of an array' +
 'has different memory addresses and is not exactly equal.');
+
+// Use slice to create a copy of a range of the elements
+// in testResults:
+console.log('Copying a range of values in testResults[]...');
+
+const storedResults2 = testResults.slice(0, 2);
+
+console.log('storedResults2[] =>');
+console.table(storedResults2);
