9bcbeec3d4ef13d7d8a7f518f53505dae048d759
Robert Hieger <rhieger@gmail.com>
2020-09-05 00:39:23 -0400

    Find index of value 1.5 using indexOf()

--- section-8-pass2/lecture-8.191/app.js
+++ section-8-pass2/lecture-8.191/app.js
@@ -2,3 +2,6 @@ const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
 
 console.log('testResults[] =>');
 console.table(testResults);
+
+console.log('Find index of 1.5...');
+console.log(`testResults.indexOf(1.5) => ${testResults.indexOf(1.5)}`);
