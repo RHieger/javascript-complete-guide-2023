f26115f51e9247c61baa669e18e68144ec1f53b0
Robert Hieger <rhieger@gmail.com>
2020-09-05 00:26:26 -0400

    Remove superfluous code and change heading in HTML for context

--- section-8-pass2/lecture-8.191/app.js
+++ section-8-pass2/lecture-8.191/app.js
@@ -2,10 +2,3 @@ const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
 
 console.log('testResults[] =>');
 console.table(testResults);
-
-console.log('Concatenating new values to store in new array...');
-
-const storedResults = testResults.concat([3.99, 2]);
-
-console.log('storedResults[] =>');
-console.table(storedResults);
