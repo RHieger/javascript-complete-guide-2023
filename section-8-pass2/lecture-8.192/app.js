1fc60ee9690774e4557003a57ea2fb480b7921bf
Robert Hieger <rhieger@gmail.com>
2020-09-05 14:31:13 -0400

    Complete Lecture 8.192: Extract index of specified object with findIndex()

--- section-8-pass2/lecture-8.192/app.js
+++ section-8-pass2/lecture-8.192/app.js
@@ -9,5 +9,17 @@ const carl = personData.find( (person, index) => {
   return person.name === 'Carl';
 });
 
+// NOTE: find() does not create a copy when it returns a value.
+// Therefore, any change made to that value is made to the actual
+// value in the array, bit to a new copy.
 console.log('Find { name: \'Carl\' }...');
 console.log('return person.name === \'Carl\' =>', carl);
+
+// Return index of { name: 'Bobby' }.
+const bobbyIndex = personData.findIndex( (person, index, personData) => {
+  return person.name === 'Bobby';
+});
+
+console.log('Find index of person.name === \'Bobby\'...');
+//NOTE: Just as with indexOf(), only first instance of object is returned.
+console.log('return person.name === Bobby =>', bobbyIndex);
