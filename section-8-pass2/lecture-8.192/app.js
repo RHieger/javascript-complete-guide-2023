552c0b4989516fc06a07c784bbe746338764555b
Robert Hieger <rhieger@gmail.com>
2020-09-05 11:42:17 -0400

    Use find() to return specified object and log to console

--- section-8-pass2/lecture-8.192/app.js
+++ section-8-pass2/lecture-8.192/app.js
@@ -4,3 +4,10 @@ const personData = [
   { name: 'Bobby', age: 58 }
 ];
 
+// Iterates over array and returns { name: 'Carl', age: 63 }
+const carl = personData.find( (person, index) => {
+  return person.name === 'Carl';
+});
+
+console.log('Find { name: \'Carl\' }...');
+console.log('return person.name === \'Carl\' =>', carl);
