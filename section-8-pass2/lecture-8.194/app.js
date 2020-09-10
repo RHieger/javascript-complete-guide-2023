ce7443505177d75689a8e67d96e91ea27d4841ea
Robert Hieger <rhieger@gmail.com>
2020-09-09 20:04:57 -0400

    Remove superfluous code and customize index.html

--- section-8-pass2/lecture-8.194/app.js
+++
@@ -1,7 +0,0 @@
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-
-console.log('testResults[] =>');
-console.table(testResults);
-
-console.log('Is 10.99 included in testResults[]?');
-console.log(`10.99 is included in testResults[] => ${testResults.includes(10.99)}`);
