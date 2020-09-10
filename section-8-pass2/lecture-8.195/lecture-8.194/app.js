75e85d15ebdeed10736b33b62269c1807d0382b7
Robert Hieger <rhieger@gmail.com>
2020-09-09 23:46:04 -0400

    Remove superfluous code and customize index.html

--- section-8-pass2/lecture-8.195/lecture-8.194/app.js
+++ section-8-pass2/lecture-8.195/lecture-8.194/app.js
@@ -1,6 +1,5 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 const tax = 0.19;
-const taxAdjustedPrices = [];
 
 prices.forEach( (price, index, array) => {
   // NOTE: 2nd property is how this was treated in the
