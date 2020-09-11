3975138a3e8df16000696d3dae4f30fa6142de1c
Robert Hieger <rhieger@gmail.com>
2020-09-11 14:32:54 -0400

    Use array.sort() to sort numbers in prices[]

--- section-8-pass2/lecture-8.196/app.js
+++ section-8-pass2/lecture-8.196/app.js
@@ -1,2 +1,26 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 
+console.log('prices[] =>');
+console.table(prices);
+
+const sortedPrices = prices.sort( (a, b) => {
+  if (a > b) {
+    // 1 signifies second operand should remain
+    // after the first.
+    return 1;
+  } else if (a === b) {
+    // 0 signifies leaving both operands where
+    // they are.
+    return 0;
+  } else {
+    // -1 signifies second operand should move
+    // before the first.
+    return -1;
+  }
+});
+
+console.log('sortedPrices[] =>');
+console.table(sortedPrices);
+
+console.log('sortedPrices[] Reversed Sorted =>');
+console.table(sortedPrices.reverse());
