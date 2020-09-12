20016e816f539c9141d89c9079877369eb511be6
Robert Hieger <rhieger@gmail.com>
2020-09-12 12:11:59 -0400

    Remove superfluous code and customize index.html

--- section-8-pass2/lecture-8.199/app.js
+++ section-8-pass2/lecture-8.199/app.js
@@ -1,13 +1,4 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
-
-// I chose filteredPrices while author chose filteredArray
-
-/* REFACTORED AS ARROW FUNCTION BELOW
-const filteredPrices = prices.filter(
-  (price, index, prices) => {
-    return price > 6.00;
-});
-*/
 const filteredPrices = prices.filter(price => price > 6.00);
 
 console.log('prices[] =>');
