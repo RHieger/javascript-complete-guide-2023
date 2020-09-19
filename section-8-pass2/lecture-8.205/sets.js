5c54008e1a372b9bcb8aa9af380a353d656d2adc
Robert Hieger <rhieger@gmail.com>
2020-09-19 17:34:04 -0400

    Create emptySet{} and ids{}; demonstrate that sets are not indexed

---
+++ section-8-pass2/lecture-8.205/sets.js
@@ -0,0 +1,9 @@
+const emptySet = new Set();
+
+console.log('This is what an empty Set object looks like.\n\n');
+console.log('emptySet => ', emptySet);
+
+const ids = new Set([1, 2, 3]);
+
+console.log('ids =>');
+console.log(ids);
