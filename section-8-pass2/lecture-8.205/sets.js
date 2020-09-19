7917c004a2e574f9fe2dd8a1ac7a3ba9ad7d6447
Robert Hieger <rhieger@gmail.com>
2020-09-19 17:46:48 -0400

    Complete Lecture 8.205: Iterate through ids{} using entries() method

--- section-8-pass2/lecture-8.205/sets.js
+++ section-8-pass2/lecture-8.205/sets.js
@@ -11,3 +11,13 @@ console.log(ids);
 // CHECK IF VALUE IS IN SET:
 
 console.log( '1 is in ids{}: ', ids.has(1) );
+
+// ITERATE THROUGH ids{} using entries() method:
+
+console.log('Iterate through ids{} using entries()...');
+
+for ( const entry of ids.entries() ) {
+  // Access only first value in entry because
+  // entries() returns the same value twice:
+  console.log( entry[0] );
+}
