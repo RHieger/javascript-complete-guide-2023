2ac5f75b3e3cf755f4ee340318ca087372dba885
Robert Hieger <rhieger@gmail.com>
2020-09-18 23:46:28 -0400

    Complete Lecture 8.202: Use spread operator to copy array of objects

--- section-8-pass2/lecture-8.202/app.js
+++ section-8-pass2/lecture-8.202/app.js
@@ -35,3 +35,51 @@ console.log('Minimum price in prices[] =>');
 const minimumPrice = Math.min(...prices);
 
 console.log(`minimumPrice => ${minimumPrice}`);
+
+// USE CASE #3:
+// REMEMBER: Objects are reference types.
+// Therefore, any changes made to a copy
+// of an object is also made to the original.
+
+const persons = [
+  { name: 'Bobby', age: 58 },
+  { name: 'Carl', age: 63}
+];
+
+const copiedPersons = [...persons];
+
+console.log('persons[] =>');
+console.table(persons);
+
+console.log('copiedPersons[] =>');
+console.table(copiedPersons);
+
+persons.push(
+  { name: 'Nathan', age: 106 }
+);
+
+console.log('Adding person to persons[]...');
+console.log('persons[] =>');
+console.table(persons);
+
+console.log('copiedPersons[] =>');
+console.table(copiedPersons);
+console.log('persons[] and copiedPersons[] have discreet values.');
+
+console.log('Changing age of persons[0]...');
+
+persons[0].age = 37;
+
+console.log('persons[] =>');
+console.table(persons);
+
+console.log('copiedPersons[] =>');
+console.table(copiedPersons);
+
+console.log(
+  'WHAT?! Why did the age change in both arrays?\n\n' + 
+  'REMEMBER. Objects are reference types. Changes ' +
+  'made to property of object in one array would ' +
+  'also be made to the same object in the ' +
+  'copied array.'
+);
