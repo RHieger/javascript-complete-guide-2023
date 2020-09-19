44493ed4b65cdcbe4bcfdb79f040a6024d86084f
Robert Hieger <rhieger@gmail.com>
2020-09-19 14:02:50 -0400

    Use array destructuring: Add variable to collect remaining elements

--- section-8-pass2/lecture-8.203/app.js
+++ section-8-pass2/lecture-8.203/app.js
@@ -13,6 +13,17 @@ console.table(nameFragments);
 const [firstName, lastName] = nameFragments;
 
 console.log('Destructuring nameFragments[]...');
-
 console.log(`[firstName, lastName] => ${firstName} ${lastName}`);
 
+// Use destructuring to list out all properties in
+// nameFragments array.
+
+const [
+  fName,
+  lName,
+  ...otherInformation
+] = nameFragments;
+
+console.log('Use array destructuring to extract all data...');
+console.log('fName, lName, ...otherInformation =>');
+console.log(fName, lName, ...otherInformation);
