cdf517284e6df5cfec55b560e391f860f1c9ca7d
Robert Hieger <rhieger@gmail.com>
2020-10-03 15:54:20 -0400

    Add personData to person2 with set()

--- section-8-pass2/lecture-8.206/maps.js
+++ section-8-pass2/lecture-8.206/maps.js
@@ -8,13 +8,28 @@ console.log('person2() => ', person2);
 console.log('Mapping additional data using object as key' +
 ' and returning nested array of key-value pairs...\n');
 
+const date1 = new Date(2020, 9, 3, 14, 3, 35);
+const date2 = new Date(2020, 8, 30, 18, 22, 37);
+
 const personData =
 new Map(
-  [ [ person1, [ {date: Date(2020, 10, 3, 14, 3, 35).toString(), price: 35} ] ] ]
+  [ [ person1, [ {date: date1.toString(), price: 35} ] ] ]
 );
 
-console.log('personData{} =>\n');
+personData.set(
+  person2,
+  [ {
+    date: date2.toString(),
+    price: 27.95
+  } ]
+);
+
+console.log('personData() =>\n');
 console.log(personData);
 
 console.log('Extracting Map data by referencing key object from personData()...\n');
 console.log(personData.get(person1));
+
+console.log('Adding to personData()...\n');
+console.log('Extracting person2 data...\n');
+console.log(personData.get(person2));
