caa09bff5ee341dc1978d3238e23f063f00262d9
Robert Hieger <rhieger@gmail.com>
2020-10-10 17:04:43 -0400

    Reduce myNumbers[] to product of its elements

--- section-8-pass2/assignment-6/assignment.js
+++ section-8-pass2/assignment-6/assignment.js
@@ -3,17 +3,19 @@ const myNumbers = [
   -39.78, 10, 1.55, 3, -66.23
 ];
 
-alert(`Contents of myNumbers[] Array:\n${myNumbers}`);
-
 const greaterThan5 = myNumbers.filter(number => number > 5);
 
 const numberObject = myNumbers.map( number => {
   return {number: number};
 });
 
+const myNumbersProduct = myNumbers.reduce( (lastValue, currentValue) => {
+  return lastValue * currentValue;
+});
+
 const nextTask = () => {
   alert("PROCEED TO NEXT TASK.");
-  console.table(greaterThan5);
+  console.clear();
 }
 
 const printTaskHeader = (taskNumber) => {
@@ -24,16 +26,27 @@ const printMappedNumbers = () => {
   numberObject.forEach(object => console.log(object));
 }
 
+const exit = () => {
+  alert("CLICK OK TO EXIT.");
+}
+
+alert(`Contents of myNumbers[] Array:\n${myNumbers}`);
+
 // EVENT LISTENERS:
 
 const task1 = document.getElementById("task1");
 task1.addEventListener("click", () => {
   printTaskHeader(1);
   console.log("myNumbers[] filtered for number > 5...");
+  console.table(greaterThan5);
   nextTask();
   printTaskHeader(1);
   console.log("myNumbers[] mapped to array of objects...\n");
   printMappedNumbers();
   nextTask();
   printTaskHeader(1);
+  console.log("myNumbers[] reduced to product of its elements...\n");
+  console.table(myNumbersProduct);
+  exit();
+  console.clear();
 });
