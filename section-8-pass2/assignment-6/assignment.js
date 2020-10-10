fe1cd2f4b1aaf9d7f120f4fc685590d07da63bf5
Robert Hieger <rhieger@gmail.com>
2020-10-10 14:14:49 -0400

    Filter myNumbers[] for number > 5: TASK #1

--- section-8-pass2/assignment-6/assignment.js
+++ section-8-pass2/assignment-6/assignment.js
@@ -3,5 +3,17 @@ const myNumbers = [
   -39.78, 10, 1.55, 3, -66.23
 ];
 
-console.log("myNumbers[] =>");
-console.table(myNumbers);
+alert(`Contents of myNumbers[] Array:\n${myNumbers}`);
+
+const greaterThan5 = myNumbers.filter(number => number > 5);
+
+// EVENT LISTENERS:
+
+const item1 = document.getElementById("task1");
+item1.addEventListener("click", () => {
+  console.log("TASK #1:\n");
+  console.log("myNumbers[] filtered for number > 5...");
+  console.table(greaterThan5);
+  alert("PROCEED TO NEXT TASK.");
+  console.clear()
+});
