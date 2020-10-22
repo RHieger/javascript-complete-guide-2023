86f8c4df741b9eb1e4f3b571338e5e7c8eac5ee8
Robert Hieger <rhieger@gmail.com>
2020-10-21 20:32:26 -0400

    Demonstrate Peculiar Behavior of var keyword

--- section-5/lecture-5.113/app.js
+++ section-5/lecture-5.113/app.js
@@ -1,5 +1,4 @@
 // Task #1: Declare a variable with let Keyword
-
 let firstName = "Bobby";
 
 console.log("Task #1: Declare a Variable with let Keyword...\n");
@@ -13,7 +12,6 @@ console.log("name =>\n");
 console.log(greet());
 
 // Task #2: Demonstrate Local Scope
-
 function greet2() {
   // Local scope variable:
   let age = 58;
@@ -34,3 +32,28 @@ function logAge()  {
 }
 
 logAge();
+
+// Task 3: Demonstrate Peculiar Behavior of var Keyword
+var myName = "Bobbyski";
+
+console.log("\nTask #3: Demonstrate Peculiar\nBehavior of var keyword...\n");
+
+console.log(`\nvar myName = ${myName}`);
+
+if (myName === "Bobbyski") {
+  var hobbies = ["Word Games", "Block Puzzles"];
+  console.log(`myName = ${myName};\nhobbies = ${hobbies}\n`);
+}
+
+console.log("\nAccess hobbies outside of if statement that spawned it...\n");
+console.log("\n", myName, hobbies);
+
+function greet3() {
+  console.log(`\nmyName: ${myName}`);
+  console.log(`hobbies[] =>\n${hobbies}`);
+}
+
+console.log("\naccess myName and hobbies[] from within function below...\n");
+console.log( "\n", greet3.toString() );
+
+greet3();
