4a554310fa8de5496a2c36da1919b635908303fa
Robert Hieger <rhieger@gmail.com>
2020-11-08 19:05:35 -0500

    Complete Lecture 6.127: Prove that functions are objects

--- section-6/lecture-6.127/app.js
+++ section-6/lecture-6.127/app.js
@@ -4,5 +4,10 @@ function startGame() {
   console.log('Game is starting...');
 }
 
+
 // addEventListener is a method of the document() Object.
 startGameBtn.addEventListener('click', startGame);
+
+console.log(`startGame() is an Object: ${typeof(startGame)}\n`);
+console.log("YES, but is a function an object? See for yourself:\n");
+console.dir(startGame);
