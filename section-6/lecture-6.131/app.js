8d963c1fb367530a49f9b9e57f4c01486c0a271e
Robert Hieger <rhieger@gmail.com>
2020-11-09 21:41:02 -0500

    Remove superfluous code

--- section-6/lecture-6.131/app.js
+++ section-6/lecture-6.131/app.js
@@ -1,19 +1,5 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-// function startGame() {
-//   console.log('Game is starting...');
-// }
-
-// ABOVE COMMENTED OUT IN FAVOR OF THE REFACTORED
-// CODE BELOW, WHICH USES AN ANONYMOUS FUNCTION:
-
-// addEventListener is a method of the document() Object.
-
-console.log("\nEvent Listener with Anonymous Function:\n");
-console.log(`startGameBtn.addEventListener('click', function() {
-  console.log("Game is starting...")
-});\n`);
-
 startGameBtn.addEventListener('click', function() {
   console.log("\nGame is starting...\n");
 });
