f079888b0aca37e6ac1cc46d90dc6e4f3d0a943e
Robert Hieger <rhieger@gmail.com>
2020-11-21 23:55:49 -0500

    Complete Step 1 for Assignment 6.4

--- section-6/assignment-6.4/assignment.js
+++ section-6/assignment-6.4/assignment.js
@@ -1,5 +1,23 @@
+// Capture nodes for assignment event listeners:
+
+const task1 = document.getElementById("task1");
+
+// TASK #1: Refactor sayHello() as Arrow Function:
+/*
 function sayHello(name) {
   console.log('Hi ' + name);
 }
+*/
 
-sayHello();
+const sayHello = (name) => {
+  console.log(`Hi, ${name}!\n`);
+};
+
+
+
+//  EVENT LISTENERS
+task1.addEventListener("click", () => {
+  console.log('\nsayHello() Function:\n');
+  console.log("\n", sayHello.toString(), "\n");
+  sayHello("Bobby");  
+});
