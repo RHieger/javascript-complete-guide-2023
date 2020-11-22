08ca096a288a519e07fab1f9f90f4dc87d4e9be3
Robert Hieger <rhieger@gmail.com>
2020-11-22 13:44:26 -0500

    Create Refactored sayHello() Function with Syntax #1 of 3

--- section-6/assignment-6.4/assignment.js
+++ section-6/assignment-6.4/assignment.js
@@ -13,11 +13,25 @@ const sayHello = (name) => {
   console.log(`Hi, ${name}!\n`);
 };
 
+// TASK #2 Refactor sayHello() with 3 Different Syntaxes
+
+const sayHello2 = (greeting, name) => {
+  console.log(`${greeting}, ${name}!\n`);
+};
+
 
 
 //  EVENT LISTENERS
 task1.addEventListener("click", () => {
+  console.clear();
   console.log('\nsayHello() Function:\n');
-  console.log("\n", sayHello.toString(), "\n");
+  console.log("\n", sayHello.toString(), "\n\n");
   sayHello("Bobby");  
+});
+
+task2.addEventListener("click",  () => {
+  console.clear();
+  console.log("\nsayHello2() Function:\n");
+  console.log("\n", sayHello2.toString(), "\n\n");
+  sayHello2("Hello there", "Bobby");
 });
