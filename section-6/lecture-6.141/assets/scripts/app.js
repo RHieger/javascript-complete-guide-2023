33282725cb2e1056a0d571a952b79dae557df29d
Robert Hieger <rhieger@gmail.com>
2020-11-28 00:14:55 -0500

    Complete Lecture 6.141: Refactor calculator app using new calculate() function

--- section-6/lecture-6.141/assets/scripts/app.js
+++ section-6/lecture-6.141/assets/scripts/app.js
@@ -33,6 +33,13 @@ function writeToLog(
     console.log(logEntries);  
   }
 
+/* THE FOLLOWING FUNCTIONS HAVE ALL BEEN
+    REFACTORED INTO THE ONE ALL-PURPOSE
+    calculate() FUNCTION BELOW.
+
+    This was accomplished creating an operator variable
+    and the bind() method.
+
 function add(num1, num2) {
   const enteredNumber = getUserNumberInput();
   const initialResult = currentResult;  // Value prior to calculation
@@ -64,29 +71,42 @@ function divide() {
   createAndWriteOutput('/', initialResult, enteredNumber);
   writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
 }
+*/
 
 function calculate(operation) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;  // Value prior to calculation
+  let operator;
   if (operation === "ADD") {
     currentResult += enteredNumber;
-    createAndWriteOutput('+', initialResult, enteredNumber);
-    writeToLog('ADD', initialResult, enteredNumber, currentResult);
+    operator = "+";
   }  else if (operation === "SUBTRACT") {
     currentResult -= enteredNumber;
-    createAndWriteOutput('-', initialResult, enteredNumber);
-    writeToLog('SUBTRACT', initialResult, enteredNumber, currentResult);
+    operator = "-";
   } else if (operation === "MULTIPLY") {
-    const initialResult = currentResult;  // Value prior to calculation
     currentResult *= enteredNumber;
-    createAndWriteOutput('*', initialResult, enteredNumber);
-    writeToLog('MULTIPLY', initialResult, enteredNumber, currentResult);
+    operator = "*";
   } else {
     currentResult /= enteredNumber;
-    createAndWriteOutput('/', initialResult, enteredNumber);
-    writeToLog('DIVIDE', initialResult, enteredNumber, currentResult);
+    operator = "/";
   }
+  createAndWriteOutput(operator, initialResult, enteredNumber);
+  writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener(
+  "click",
+  calculate.bind(this, "ADD")
+);
+subtractBtn.addEventListener(
+  "click",
+  calculate.bind(this, "SUBTRACT")
+);
+multiplyBtn.addEventListener(
+  "click",
+  calculate.bind(this, "MULTIPLY")
+);
+divideBtn.addEventListener(
+  "click",
+  calculate.bind(this, "DIVIDE")
+);
