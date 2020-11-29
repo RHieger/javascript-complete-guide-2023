351fdb579a8a1144f37f542a48fb657b8db49445
Robert Hieger <rhieger@gmail.com>
2020-11-29 12:59:51 -0500

    Complete Assignment 7.5: Create Event Listener to Change textContent of <h1>

--- section-7/assignment-7.5/assignment.js
+++ section-7/assignment-7.5/assignment.js
@@ -6,8 +6,9 @@
 const taskSelection1 = document.getElementById("task-1");
 const taskSelection2 = document.querySelector("li");
 const taskSelection3 = document.getElementById("task-2");
+const taskSelection4 = document.getElementById("task-3");
 
-// TASK #2: Change Document Title in <head> to "Assignment Solved"
+// TASK #2: Change Document Title in <head> to "Assignment - Solved!"
 //                  using two different means of querying the <title> element,
 //                  one using the document property, and the other using
 //                  the specific element property in which <title> resides.
@@ -15,6 +16,11 @@ const taskSelection3 = document.getElementById("task-2");
 // Capture Task #2 Element in 2 Different Ways:
 const windowTitle1 = document.head.querySelector("title");
 const windowTitle2 = document.head;
+
+// TASK #3: Select <h1> and change its content to "Assignment - Solved!"
+
+// Capture Task #3 Element:
+const mainHead = document.querySelector("h1");
 
 // EVENT LISTENERS:
 
@@ -28,4 +34,8 @@ taskSelection2.addEventListener("click", () => {
 
 taskSelection3.addEventListener("click", () => {
   windowTitle1.textContent = "Assignment - Solved!";
+});
+
+taskSelection4.addEventListener("click", () => {
+  mainHead.textContent = "Assignment - Solved!";
 });
