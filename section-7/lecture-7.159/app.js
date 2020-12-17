7160eb128ac6bca64d6f1a99edf02d0ff099001c
Robert Hieger <rhieger@gmail.com>
2020-12-16 20:57:40 -0500

    Select first element child of <ul>

---
+++ section-7/lecture-7.159/app.js
@@ -0,0 +1,9 @@
+const uList = document.body
+              .firstElementChild
+              .nextElementSibling;
+
+const firstListItem = uList.firstElementChild;
+
+// TASK #1: Select first element child of <ul>
+console.log("Select the first element child of <ul> =>\n");
+console.log(firstListItem, "\n");
