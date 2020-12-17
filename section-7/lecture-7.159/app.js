355751fd55e3736ebb674bdd42c1673f5ca46d67
Robert Hieger <rhieger@gmail.com>
2020-12-16 21:09:17 -0500

    Complete Lecture 7.159: Select first element child of <ul> using querySelector()

--- section-7/lecture-7.159/app.js
+++ section-7/lecture-7.159/app.js
@@ -3,7 +3,13 @@ const uList = document.body
               .nextElementSibling;
 
 const firstListItem = uList.firstElementChild;
+const uList2 = document.querySelector("ul");
 
 // TASK #1: Select first element child of <ul>
 console.log("Select the first element child of <ul> =>\n");
 console.log(firstListItem, "\n");
+
+// TASK #2: Select first element child of <ul>
+// using QuerySelector()
+console.log("Select first element child of <ul> using querySelector()...\n");
+console.log(uList2.firstElementChild, "\n");
