524b48fdb4c2740203caddda2d9221df44e5402f
Robert Hieger <rhieger@gmail.com>
2020-12-19 18:13:24 -0500

    Complete Lecture 7.162: Insert Adjacent Child Element Using insertAdjacentHTML

--- section-7/lecture-7.162/app.js
+++ section-7/lecture-7.162/app.js
@@ -1,2 +1,7 @@
 const section = document.querySelector('section');
+const uList = document.querySelector('ul');
 const addElement = document.getElementById('add');
+
+addElement.addEventListener('click', () => {
+  uList.insertAdjacentHTML('beforeend', '<li class="list-item">New Item</li>');
+});
