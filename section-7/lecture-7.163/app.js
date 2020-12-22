1b9323b14953c9f1bf6519f44ae336ce792f8aae
Robert Hieger <rhieger@gmail.com>
2020-12-22 18:53:51 -0500

    Complete Lecture 7.163: Add Element Using createElement()

--- section-7/lecture-7.163/app.js
+++ section-7/lecture-7.163/app.js
@@ -2,3 +2,8 @@ const section = document.querySelector('section');
 const uList = document.querySelector('ul');
 const addElement = document.getElementById('add');
 
+addElement.addEventListener('click', () => {
+  const newItem = document.createElement('li');
+  newItem.textContent = 'New Item';
+  uList.appendChild(newItem);
+});
