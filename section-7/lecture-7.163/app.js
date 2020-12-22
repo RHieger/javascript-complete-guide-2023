1fbfec20d23d41dc7373714bf067aa4167b7c69f
Robert Hieger <rhieger@gmail.com>
2020-12-22 18:42:03 -0500

    Remove superfluous code

--- section-7/lecture-7.163/app.js
+++ section-7/lecture-7.163/app.js
@@ -2,6 +2,3 @@ const section = document.querySelector('section');
 const uList = document.querySelector('ul');
 const addElement = document.getElementById('add');
 
-addElement.addEventListener('click', () => {
-  uList.insertAdjacentHTML('beforeend', '<li class="list-item">New Item</li>');
-});
