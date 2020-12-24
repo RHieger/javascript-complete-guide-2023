0492105f95734e22d1b0468546f2755c9eab7b48
Robert Hieger <rhieger@gmail.com>
2020-12-24 13:22:16 -0500

    Add 'Replace First Item' button to index.html

--- section-7/lecture-7.163/app.js
+++ section-7/lecture-7.163/app.js
@@ -1,10 +1,20 @@
 const section = document.querySelector('section');
 const uList = document.querySelector('ul');
 const addElement = document.getElementById('add');
+const addBeforeLast = document.getElementById('beforeLast');
+const replaceFirst = document.getElementById('replaceFirstItem');
+
+const newItem = document.createElement('li');
+newItem.textContent = 'New Item';
 
 addElement.addEventListener('click', () => {
-  const newItem = document.createElement('li');
-  newItem.textContent = 'New Item';
   // TASK #1: Prepend newItem:
   uList.prepend(newItem);
+});
+
+addBeforeLast.addEventListener('click', () => {
+  // TASK #2 Insert newItem before last item
+  // using lastElementChild property and
+  // before() method:
+  uList.lastElementChild.before(newItem);
 });
