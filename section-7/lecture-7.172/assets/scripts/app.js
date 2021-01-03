b919c4718222b0ba1b0f725d217cc9fa63879bc0
Robert Hieger <rhieger@gmail.com>
2021-01-03 10:13:39 -0500

    Complete Lecture 7.172: toggle addMovieModal

--- section-7/lecture-7.172/assets/scripts/app.js
+++ section-7/lecture-7.172/assets/scripts/app.js
@@ -1,2 +1,8 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
