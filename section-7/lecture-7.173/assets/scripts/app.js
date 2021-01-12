fb95d25c38d9c2914e6dffd74ac5b16b4fa3e18e
Robert Hieger <rhieger@gmail.com>
2021-01-11 21:32:11 -0500

    Complete Lecture 7.173: Add cancelAddMovie() function

--- section-7/lecture-7.173/assets/scripts/app.js
+++ section-7/lecture-7.173/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 
 const toggleBackDrop = () => {
   backdrop.classList.toggle('visible');
@@ -11,5 +12,14 @@ const toggleMovieModal = () => {
   toggleBackDrop();
 };
 
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
