823cb3bca13061028d1f6a753112cff9680f687e
Robert Hieger <rhieger@gmail.com>
2021-01-11 21:12:22 -0500

    Add event listener to backdrop object to dismiss modal

--- section-7/lecture-7.173/assets/scripts/app.js
+++ section-7/lecture-7.173/assets/scripts/app.js
@@ -2,13 +2,14 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 
-const togglBackDrop = () => {
+const toggleBackDrop = () => {
   backdrop.classList.toggle('visible');
 };
 
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
-  togglBackDrop();
+  toggleBackDrop();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
