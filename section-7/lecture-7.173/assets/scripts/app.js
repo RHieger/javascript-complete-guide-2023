d0bd61fd8657a0b34bc93249aff804b95f2aaf6b
Robert Hieger <rhieger@gmail.com>
2021-01-11 20:41:40 -0500

    Create toggleBackDrop() function

--- section-7/lecture-7.173/assets/scripts/app.js
+++ section-7/lecture-7.173/assets/scripts/app.js
@@ -2,8 +2,13 @@ const addMovieModal = document.getElementById('add-modal');
 const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 
+const togglBackDrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
 const toggleMovieModal = () => {
   addMovieModal.classList.toggle('visible');
+  togglBackDrop();
 };
 
 startAddMovieButton.addEventListener('click', toggleMovieModal);
