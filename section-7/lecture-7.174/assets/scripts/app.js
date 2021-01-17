b6a9c05d5222328e1ca9de34ae58efbeace54a8b
Robert Hieger <rhieger@gmail.com>
2021-01-16 20:09:46 -0500

    Migrate code from lecture-7.173 to lecture-7.174

---
+++ section-7/lecture-7.174/assets/scripts/app.js
@@ -0,0 +1,25 @@
+const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+const toggleBackDrop = () => {
+  backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
+  addMovieModal.classList.toggle('visible');
+  toggleBackDrop();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', toggleMovieModal);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie);
