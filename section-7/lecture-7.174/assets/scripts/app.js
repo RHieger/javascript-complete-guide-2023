216175e308e4395373c67516d2c0be672287c2d2
Robert Hieger <rhieger@gmail.com>
2021-01-16 20:48:12 -0500

    Complete Lecture 7.174: Implement addMovieHandler() function

--- section-7/lecture-7.174/assets/scripts/app.js
+++ section-7/lecture-7.174/assets/scripts/app.js
@@ -22,6 +22,18 @@ const addMovieHandler = () => {
   const titleValue = userInputs[0].value;
   const imageUrlValue = userInputs[1].value;
   const ratingValue = userInputs[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
+    ratingValue.trim () === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert(
+      'Please enter valid values and rating between 1 and 5.'
+      );
+  }
 };
 
 const backdropClickHandler = () => {
