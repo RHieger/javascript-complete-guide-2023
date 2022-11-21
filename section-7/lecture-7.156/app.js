32d313ab3c3a95c1af2f4289bea44653c4821573
Robert Hieger <rhieger@gmail.com>
2022-11-21 14:39:07 -0500

    Complete Lecture 7.156: Change textContent and styling of <h1>

---
+++ section-7/lecture-7.156/app.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1');
+
+// Change content of h1 tag:
+h1.textContent= 'Some new title!';
+
+// Change styling of h1 tag:
+h1.style.color = '#ffffff';   // white
+h1.style.backgroundColor = '#000000'; // black
