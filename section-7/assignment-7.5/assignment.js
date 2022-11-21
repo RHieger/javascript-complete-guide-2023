79bab4f31267fb0498f807d91a0adcca0399b87d
Robert Hieger <rhieger@gmail.com>
2022-11-20 19:56:24 -0500

    Create assignment.js stub including documentation of objectives

---
+++ section-7/assignment-7.5/assignment.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Assignment 7.5
+ * 
+ * Robert Hieger
+ * 11/20/2022
+ * 
+ * OBJECTIVES:
+ * 
+ * 1. Select first list item (in two different ways at least!)
+ *    and change the background-color to black, text-color
+ *    to white.
+ * 
+ * 2. Change the document title (in <head>) to "Assignment - Solved!".
+ *    Use two different ways for getting access to the <title>
+ *    element: Via querySelector on document and via querySelector
+ *    on the certain property you find in document.
+ * 
+ * 3. Select the <h1> element in index.html and change it's text to
+ *    "Assignment&mdash;Solved!"
+ * 
+ */
