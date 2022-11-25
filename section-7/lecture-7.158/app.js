b04a4bb8f894454b149a9cd66dbe0d4115ad52c6
Robert Hieger <rhieger@gmail.com>
2022-11-24 21:34:02 -0500

    Add DOM node objects for li and insert comment documentation

--- section-7/lecture-7.158/app.js
+++ section-7/lecture-7.158/app.js
@@ -2,3 +2,20 @@
 // previousSiblingNode, previousElementSiblingNode,
 // nextSiblingNode and nextElementSiblingMode.
 
+// Change h1 heading and its styling
+const h1 = document.querySelector('h1');
+h1.textContent = 'Some new title!';
+
+h1.style.color = '#ffffff';             // white
+h1.style.backgroundColor = '#000000';   // black
+
+// Capture first li element
+// NOTE: Because querySelector() only returns the
+// first match it finds, the <li> returned is the
+// first one within the <ul> tags.
+const li = document.querySelector('li');
+
+// Capture previous node
+// NOTE: The previousNode() method returns node
+// regardless of its type, so because the previous
+// node to li is a text node, it is returned.
