57d71b6d808f4ddcb3f98b9806ccb4d10a8fc755
Robert Hieger <rhieger@gmail.com>
2022-11-26 21:30:47 -0500

    Lecture 7.164: Demonstrate appending more than 1 child node with append()

--- section-7/lecture-7.164/app.js
+++ section-7/lecture-7.164/app.js
@@ -1,19 +1,14 @@
 /**
  * 
- * Lecture 7.163: Adding Elements via createElement()
+ * Lecture 7.164: Inserting DOM Elements
  * 
  * Robert Hieger
  * 11/26/2022
  * 
- * OBJECTIVE: Dynamically inject an HTML element as a
- * sibling to those occuring within a <ul> element.
- * Naturally, the logical insertion would be an <li>
- * element, setting its text content to "Item 4."
- * 
- * Additionally, set the font size of the new <li> element
- * to 21px and set its color to blue. Finally append the
- * new element to the <ul> to make it visible within the
- * browser window.
+ * OBJECTIVE: Explore the difference between appendChild()
+ * and append() methods. Specifically, this application
+ * demonstrates how append() can append more than 1 item,
+ * unlike appendChild().
  * 
  */
 
@@ -28,15 +23,23 @@ const button = document.querySelector('button');
 
 // Build and append new list item to <ul>:
 
-// Create new <li> element
-const newListItem = document.createElement('li');
-// Add text node to newListItem
-newListItem.textContent = 'Item 4';
-// Set color to blue and font size to 21px
-newListItem.style.color = 'blue';
-newListItem.style.fontSize = '21px';
-// Append newListItem to list
-list.appendChild(newListItem);
+// Create new <li> elements
+const newLi1 = document.createElement('li');
+const newLi2 = document.createElement('li');
+
+// Set text nodes for both <li> elements
+newLi1.textContent = 'Item 4';
+newLi2.textContent = 'Item 5';
+
+// Set styling for new <li> element nodes
+newLi1.style.color = 'blue';
+newLi1.style.fontSize = '21px';
+newLi2.style.color = 'blue';
+newLi2.style.fontSize = '21px';
+
+// append() method appends both <li> elements
+// at the same time, unlike appendChild()
+list.append(newLi1, newLi2);
 
 button.addEventListener('click', () => {
   section.classList.toggle('invisible');
