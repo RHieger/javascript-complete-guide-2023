6db34c5a2baaa3d042974809e0f2096be0eae263
Robert Hieger <rhieger@gmail.com>
2022-11-29 18:49:56 -0500

    Lecture 7.172: Migrate Code from Lecture-7.171

---
+++ section-7/lecture-7.172/scripts/app.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * Lecture 7.171: Selecting the Modal and "Add" Button
+ * 
+ * Robert Hieger
+ * 11/29/2022
+ * 
+ * OBJECTIVE: TBD
+ * 
+ */
+
+// DOM Objects
+
+const addMovieModal = document.getElementById('add-modal');
+const addStartMovieButton = document.querySelector('header button');
+
