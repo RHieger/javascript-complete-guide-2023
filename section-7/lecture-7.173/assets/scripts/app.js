e4461bf0181a2e8dad2977d45438312b15c4878b
Robert Hieger <rhieger@gmail.com>
2022-11-30 00:07:21 -0500

    Complete Lecture 7.173: Add Event Listeners that open and close modal

--- section-7/lecture-7.173/assets/scripts/app.js
+++ section-7/lecture-7.173/assets/scripts/app.js
@@ -10,6 +10,17 @@
  * purpose is to hide the modal if it is visible or reveal it
  * when it is hidden.
  * 
+ * Additionally, declare another EventListner on backDrop node.
+ * This EventListener will make a callback to a new function
+ * called backdropClickHandler, which will hide the backdrop
+ * by making an internal reference to toggleMovieModal, which
+ * in turn calls toggleBackDrop.
+ * 
+ * The end result should be that clicking on the backdrop after
+ * opening the modal will hide the modal and remove the backdrop.
+ * Clicking the "Cancel" button on modal will also hide the modal
+ * and remove the backdrop.
+ * 
  */
 
 // DOM Objects
@@ -17,20 +28,36 @@ const addMovieModal = document.getElementById('add-modal');
 const addStartMovieButton =
   document.querySelector('header button');
 const backDrop = document.getElementById('backdrop');
+const cancelAddMovieButton =
+  addMovieModal.querySelector('.btn--passive');
 
 // Callback Functions
+const toggleBackDrop = () => {
+  backDrop.classList.toggle('visible');
+};
+
 const toggleMovieModal = () => {
   // Hide modal if present; display if hidden.
   addMovieModal.classList.toggle('visible');
+  toggleBackDrop();
 };
 
-const toggleBackDrop = () => {
-  backDrop.classList.toggle('visible');
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
+
+const cancelAddMovie = () => {
+  toggleMovieModal();
 };
 
 // EventListeners
 addStartMovieButton
   .addEventListener('click', () => {
     toggleMovieModal();
-    toggleBackDrop();
   });
+
+backDrop
+  .addEventListener( 'click', backdropClickHandler );
+
+cancelAddMovieButton
+  .addEventListener('click', cancelAddMovie);
