cacbbbc883706be3fcd00f1a7ae4dd56c3ef60a7
Robert Hieger <rhieger@gmail.com>
2022-12-01 00:19:41 -0500

    Complete Lecture 7.175
    Implement creation of movie objects to be stored in movies array. Handle graceful opening and closing of modal.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-7/lecture-7.175/assets/scripts/app.js
+++ section-7/lecture-7.175/assets/scripts/app.js
@@ -9,7 +9,18 @@
  * OBJECTIVE: Declare a Datastore Array to contain
  * movie objects.
  * 
- * TBD
+ * In addMovieHandler, create a movie object that
+ * will be pushed to the movies datastore array.
+ * The results of this push will be logged to the
+ * console at this stage in the application's
+ * development. Later, it will be rendered on
+ * the web page.
+ * 
+ * Finally, when the "Add" button is clicked on the
+ * add movie modal, the modal box fields should be
+ * emptied and the modal and backdrop removed from
+ * the screen. This is accomplished by making a
+ * call to toggleMovieModal().
  * 
  */
 
@@ -24,33 +35,48 @@ const confirmAddMovieButton =
   cancelAddMovieButton.nextElementSibling;
 const userInput = addMovieModal.querySelectorAll('input');
 
+// Declare Datastore to contain movie objects:
+const movies = [];
+
 // Callback Functions
 const toggleBackDrop = () => {
   backDrop.classList.toggle('visible');
 };
 
+// Hides modal if present; displays if hiddenx
 const toggleMovieModal = () => {
-  // Hide modal if present; display if hidden.
   addMovieModal.classList.toggle('visible');
   toggleBackDrop();
 };
 
-const cancelAddMovieHandler  = () => {
-  toggleMovieModal();
+// Empties movie input fields
+const clearMovieInput = () => {
+  for (const input of userInput) {
+    input.value = '';
+  }
 };
 
+// Removes Modal and backdrop on "Cancel" button click
+const cancelAddMovieHandler  = () => {
+  toggleMovieModal();
+  clearMovieInput();
+};
+
+// Removes backdrop any time modal closes
 const backdropClickHandler = () => {
   toggleMovieModal();
 };
 
+// Handles creation of movie objects to be stored
+// in movies array.
 const addMovieHandler = () => {
   const titleValue = userInput[0].value;
-  const imgUrlValue = userInput[1].value;
+  const imageUrlValue = userInput[1].value;
   const ratingValue = userInput[2].value;
 
   if (
     titleValue.trim() === '' ||
-    imgUrlValue.trim() === '' ||
+    imageUrlValue.trim() === '' ||
     ratingValue.trim() === '' ||
     +ratingValue < 1 ||
     +ratingValue > 5
@@ -61,6 +87,24 @@ const addMovieHandler = () => {
     );
     return;
   }
+
+  // Holds movie object to be pushed to movies array:
+  const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+  };
+
+  movies.push(newMovie);
+
+  // For Diagnostic purposes only
+  console.log(movies);
+
+  // Removes modal and backdrop
+  toggleMovieModal();
+  
+  // Clear modal input fields
+  clearMovieInput();
 };
 
 // EventListeners
