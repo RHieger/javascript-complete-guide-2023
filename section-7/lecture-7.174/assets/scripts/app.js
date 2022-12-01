ca850260bea6166c8f10aeb1bb8ada71fe119f8d
Robert Hieger <rhieger@gmail.com>
2022-11-30 20:21:52 -0500

    Complete Lecture 7.174
    Capture user input from modal, perform basic validation of the user input to each field, and display an alert box if any of the conditions is not met.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-7/lecture-7.174/assets/scripts/app.js
+++ section-7/lecture-7.174/assets/scripts/app.js
@@ -1,25 +1,33 @@
 /**
  * 
- * Lecture 7.173: Opening a Modal by Changing CSS Classes
+ * Lecture 7.174: Fetching and Validating User Input
  * 
  * Robert Hieger
- * 11/29/2022
+ * 11/30/2022
  * 
- * OBJECTIVE: Create EventListener to activate modal. Declare
- * named callback function to be used by event listener whose
- * purpose is to hide the modal if it is visible or reveal it
- * when it is hidden.
+ * OBJECTIVE: Declare a DOM object to capture the "Add"
+ * button in the movie modal. Create an event listener
+ * that triggers upon clicking the "Add" button and does
+ * a basic validation on the user input to make sure of
+ * the following:
  * 
- * Additionally, declare another EventListner on backDrop node.
- * This EventListener will make a callback to a new function
- * called backdropClickHandler, which will hide the backdrop
- * by making an internal reference to toggleMovieModal, which
- * in turn calls toggleBackDrop.
+ * 1. That all inputs are not blank.
+ * 2. That any individual input is not blank.
+ * 3. That user input for the movie rating is an
+ *    integer between 1 and 5. Less than 1 will
+ *    return false, as will 6 or -3, for example.
+ * 4. If any of the above conditions evaluate to
+ *    false, an alert box is triggered with a
+ *    message that the rating must be between
+ *    1 and 5.
  * 
- * The end result should be that clicking on the backdrop after
- * opening the modal will hide the modal and remove the backdrop.
- * Clicking the "Cancel" button on modal will also hide the modal
- * and remove the backdrop.
+ * This alert box is limited at this point in that no
+ * matter what the error, it only speaks of the movie
+ * rating being incorrect.
+ * 
+ * I can imagine a refactoring that might include a
+ * switch statement that would display different
+ * alert boxes for different errors.
  * 
  */
 
@@ -30,6 +38,9 @@ const addStartMovieButton =
 const backDrop = document.getElementById('backdrop');
 const cancelAddMovieButton =
   addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton =
+  cancelAddMovieButton.nextElementSibling;
+const userInput = addMovieModal.querySelectorAll('input');
 
 // Callback Functions
 const toggleBackDrop = () => {
@@ -42,12 +53,32 @@ const toggleMovieModal = () => {
   toggleBackDrop();
 };
 
+const cancelAddMovieHandler  = () => {
+  toggleMovieModal();
+};
+
 const backdropClickHandler = () => {
   toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
-  toggleMovieModal();
+const addMovieHandler = () => {
+  const titleValue = userInput[0].value;
+  const imgUrlValue = userInput[1].value;
+  const ratingValue = userInput[2].value;
+
+  if (
+    titleValue.trim() === '' ||
+    imgUrlValue.trim() === '' ||
+    ratingValue.trim() === '' ||
+    +ratingValue < 1 ||
+    +ratingValue > 5
+  ) {
+    alert(
+      'Please enter valid values' +
+      ' (rating between 1 and 5).'
+    );
+    return;
+  }
 };
 
 // EventListeners
@@ -60,4 +91,7 @@ backDrop
   .addEventListener( 'click', backdropClickHandler );
 
 cancelAddMovieButton
-  .addEventListener('click', cancelAddMovie);
+  .addEventListener('click', cancelAddMovieHandler);
+
+confirmAddMovieButton
+  .addEventListener('click', addMovieHandler);
