b64b14b23040136b1c57ce6cc9ef3abadfcbe821
Robert Hieger <rhieger@gmail.com>
2022-11-30 23:31:03 -0500

    Begin Embedded Documentation
    Remove old documentation from migrated code in
    index.html and app.js.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-7/lecture-7.175/assets/scripts/app.js
+++ section-7/lecture-7.175/assets/scripts/app.js
@@ -1,33 +1,15 @@
 /**
  * 
- * Lecture 7.174: Fetching and Validating User Input
+ * Lecture 7.175: Creating a Movie in JavaScript
+ *                and Clearing the Input
  * 
  * Robert Hieger
  * 11/30/2022
  * 
- * OBJECTIVE: Declare a DOM object to capture the "Add"
- * button in the movie modal. Create an event listener
- * that triggers upon clicking the "Add" button and does
- * a basic validation on the user input to make sure of
- * the following:
+ * OBJECTIVE: Declare a Datastore Array to contain
+ * movie objects.
  * 
- * 1. That all inputs are not blank.
- * 2. That any individual input is not blank.
- * 3. That user input for the movie rating is an
- *    integer between 1 and 5. Less than 1 will
- *    return false, as will 6 or -3, for example.
- * 4. If any of the above conditions evaluate to
- *    false, an alert box is triggered with a
- *    message that the rating must be between
- *    1 and 5.
- * 
- * This alert box is limited at this point in that no
- * matter what the error, it only speaks of the movie
- * rating being incorrect.
- * 
- * I can imagine a refactoring that might include a
- * switch statement that would display different
- * alert boxes for different errors.
+ * TBD
  * 
  */
 
