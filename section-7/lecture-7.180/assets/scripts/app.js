89b2b2f6d56d6ce2c4da3d73f7708b4dd8eb4d41
Robert Hieger <rhieger@gmail.com>
2022-12-09 18:57:58 -0500

    Lecture 7.180: Added Needed DOM Objects
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-7/lecture-7.180/assets/scripts/app.js
+++ section-7/lecture-7.180/assets/scripts/app.js
@@ -25,3 +25,11 @@
 
 
 // DOM Objects
+const addMovieModal = document.getElementById('add-modal');
+const startAddMovieButton = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelector('input');
+const textEntrySection = document.getElementById('entry-text-section');
+const deleteMovieModal = document.getElementById('delete-modal');
