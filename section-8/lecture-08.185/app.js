b3faa83aacd6d2d3a3ac0ecf72ea9ac81f7bda54
Robert Hieger <rhieger@gmail.com>
2022-12-11 20:48:52 -0500

    Lecture 8.185: Implement an Array from a Constructor
    Explore 3 different variations that show different characteristics of the Array() constructor.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.185/app.js
+++ section-8/lecture-08.185/app.js
@@ -13,3 +13,16 @@
 // Method #1: Most commonly used
 const numbers = [1, 2, 3];
 console.log(numbers);
+
+// Method #2: Instantiating an Array Object
+const moreNumbers = new Array();  // instantiates blank array
+console.log(moreNumbers);
+
+const evenMoreNumbers = new Array(
+  1, 5
+);  // Passing two elements to array constructor
+console.log(evenMoreNumbers);
+
+const yetMoreNumbers = new Array(5);  // instantiats array with
+                                      // 5 empty elements
+console.log(yetMoreNumbers);
