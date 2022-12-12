96c2285f39e9bb12cfcd1a0d154a7baf1da70b06
Robert Hieger <rhieger@gmail.com>
2022-12-11 20:38:34 -0500

    Lecture 8.185: Implemented First Array Creation Method
    Use object literal notation for Array object
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

---
+++ section-8/lecture-08.185/app.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Lecture 8.185: Creating Arrays
+ * 
+ * Robert Hieger
+ * 12/11/2022
+ * 
+ * Objective: Explore different methods of creating
+ *            arrays in JavaScript.
+ * 
+ */
+
+// Method #1: Most commonly used
+const numbers = [1, 2, 3];
+console.log(numbers);
