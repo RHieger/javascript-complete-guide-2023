aab9247f5f41dbed7359109d0c31e83faf221619
Robert Hieger <rhieger@gmail.com>
2022-12-12 00:14:21 -0500

    Remove Superfluous Code and Begin Embedded Documentation

--- section-8/lecture-08.187/app.js
+++ section-8/lecture-08.187/app.js
@@ -1,43 +1,13 @@
 /**
  * 
- * Lecture 8.185: Creating Arrays
+ * Lecture 8.187: push(), pop(), unshift(), shift()--
+ * Adding and Removing Elements
  * 
  * Robert Hieger
- * 12/11/2022
+ * 12/12/2022
  * 
- * Objective: Explore different methods of creating
- *            arrays in JavaScript.
+ * Objective: Explore and demonstrate the above four
+ * methods exposed by the Array() object.
  * 
  */
 
-// Method #1: Most commonly used and fastest performance
-const numbers = [1, 2, 3];
-console.log(numbers);
-
-// Method #2: Instantiating an Array Object
-const moreNumbers = new Array();  // instantiates blank array
-console.log(moreNumbers);
-
-const evenMoreNumbers = new Array(
-  1, 5
-);  // Passing two elements to array constructor
-console.log(evenMoreNumbers);
-
-const yetMoreNumbers = new Array(5);  // instantiats array with
-                                      // 5 empty elements
-console.log(yetMoreNumbers);
-
-// Method #3: Instantiating an Array Object with Syntactic Sugar
-  // same as new Array()—assumed implicitly
-const enoughNumbers = Array(5, 2);
-console.log(enoughNumbers);
-
-// Method #4: Instantiate an Array Object with Specified Elements
-const guessAgain = Array.of(1, 2);
-console.log(guessAgain);
-
-// Method #5 Convert Array-Like Object to Array
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
-const guessYetAgain = Array.from(listItems);  
-console.log(guessYetAgain);
