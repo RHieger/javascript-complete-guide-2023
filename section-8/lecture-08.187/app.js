749a0a8e4c13a8324638f4360ee18da25f5b9474
Robert Hieger <rhieger@gmail.com>
2022-12-13 07:17:12 -0500

    Lecture 8.178: Demonstrate shift() Method
    Shifts all array elements to the left deleting the one at index 0.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.187/app.js
+++ section-8/lecture-08.187/app.js
@@ -4,7 +4,7 @@
  * Adding and Removing Elements
  * 
  * Robert Hieger
- * 12/12/2022
+ * 12/13/2022
  * 
  * Objective: Explore and demonstrate the above four
  * methods exposed by the Array() object.
@@ -34,5 +34,12 @@ console.log('\t', hobbies);
 hobbies.pop();
 console.log('\n\tUse pop() to Delete Element ' +
             'from the End of the Array'
+);
+console.log('\t', hobbies);
+
+// Method #4: shift()—deletes the first element in an array.
+hobbies.shift();
+console.log('\n\tUse shift() to Delete Element ' +
+            'from the Beginning of the Array'
 );
 console.log('\t', hobbies);
