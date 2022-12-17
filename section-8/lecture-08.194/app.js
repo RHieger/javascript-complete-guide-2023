b65df04c7331087352390fbaab4bda9eb45dd807
Robert Hieger <rhieger@gmail.com>
2022-12-17 17:42:29 -0500

    Lecture 8.194: Delete Superfluous Code
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.194/app.js
+++ section-8/lecture-08.194/app.js
@@ -1,31 +1,12 @@
 /**
  * 
- * Lecture 8.193: Is it Included?
+ * Lecture 8.194: Alternative to for Loops:
+ * The forEach() Method
  * 
  * Robert Hieger
  * 12/17/2022
  * 
- * Objective: Demonstrate the use of the included() method,
- * which searches an array to see if a specified value is
- * included within it. The function returns true or false,
- * but does not return the object itself.
+ * Objective: Demonstrate the utility of the forEach()
+ * method and explore its differences from for().
  * 
  */
-
-// testResults revisited:
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-
-console.log('\n\n\tDEMONSTRATION OF THE includes() METHOD:');
-console.log('\t--------------------------------------');
-
-console.log('\n\tLet\'s revisit the testResults[] Array:');
-console.log('\t', testResults);
-
-console.log(
-  '\n\tNow let\'s test to see whether 10.99 is included' +
-  ' in the elements of testResults[]:'
-);
-
-const elementIsPresent = testResults.includes(10.99);
-
-console.log('\n\t10.99 is included: ', elementIsPresent);
