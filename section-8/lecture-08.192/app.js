25bdcd07791d1316a63dbb27038bb7e5c17a2e8e
Robert Hieger <rhieger@gmail.com>
2022-12-17 16:55:05 -0500

    Complete Lecture 8.192
    Use findIndex() to find the index for the object whose value is “Max.”
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.192/app.js
+++ section-8/lecture-08.192/app.js
@@ -39,9 +39,23 @@ const manuel = personData
     return person.name === 'Manuel';
   });
 
-  // Log manuel() to console.
-  console.log(
-    '\n\tUsing the find() method, now let\'s find the object' +
-    ' whose value is Manuel and log it to the console.'
-  );
-  console.log('\tObject found: ', manuel);
+// Log manuel() to console.
+console.log(
+  '\n\tUsing the find() method, now let\'s find the object' +
+  ' whose value is Manuel and log it to the console.'
+);
+console.log('\tObject found: ', manuel);
+
+// Use findIndex to locate the index at which the object
+// whose value is "Max" resides.
+const maxIndex = personData
+.findIndex( (person, index, people) => {
+  return person.name === 'Max';
+});
+
+console.log(
+  '\n\tNow using the findIndex() method, let\'s find the index' +
+  ' for the object whose value is \'Max\' and log it to' +
+  ' the console.'
+);
+console.log('\tIndex for Max: ', maxIndex);
