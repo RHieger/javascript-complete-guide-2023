ecbcbc2f64bccb471c989ee6e62bcf95420e7079
Robert Hieger <rhieger@gmail.com>
2022-12-16 21:19:22 -0500

    Lecture 8.191: Demonstrate Limitation of indexOf()
    Demonstrate that indexOf() only returns the index for the first match. If there is another instance, it is ignored by this method.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.191/app.js
+++ section-8/lecture-08.191/app.js
@@ -23,3 +23,21 @@ console.log('\t', testResults);
 // Use indexOf() to find index of 1.5 in testResults.
 console.log('\n\tUse indexOf() to find index of 1.5:');
 console.log('\t', testResults.indexOf(1.5));
+
+// Demonstrate that indexOf() only returns index of
+// the first match.
+
+console.log(
+  '\n\tindexOf() only returns index of first\n' +
+  ' match. Watch this!'
+);
+
+// Insert value of 1.5 after 10.99 in testResults.
+testResults.splice(4, 0, 1.5);
+
+console.log('\n\tLet\'s insert 1.5 in an element after 10.99.');
+console.log('\tHere\'s the edited array:');
+console.log('\n\t', testResults);
+
+console.log('\n\tNow let\'s get the index of 1.5 again:');
+console.log('\tIndex of 1.5: ', testResults.indexOf(1.5));
