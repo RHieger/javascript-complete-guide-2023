42f82c95a60e80949f4ac2483f6152398548fd68
Robert Hieger <rhieger@gmail.com>
2022-12-17 19:29:54 -0500

    Lecture 8.194: Logged Contents of prices[] to Console

--- section-8/lecture-08.194/app.js
+++ section-8/lecture-08.194/app.js
@@ -16,6 +16,35 @@
 // Our new array for manipulation:
 const prices = [10.99, 5.99, 3.99, 6.59];
 
+console.log('\n\n\tforEach() USE CASE DEMO:');
+console.log('\t-----------------------');
+
+// Log prices to console:
+console.log('\n\tContents of prices[]:');
+console.table(prices);
+
+console.log(
+  '\n\tNow we use the forEach() Loop to calculate the' +
+  ' grand total including sales tax.'
+);
+
+console.log(
+  '\n\tBe sure to check the code in app.js to see how ' +
+  'this was accomplished.'
+);
+
 // Tax Rate:
 const salesTax = 0.08875;
 const taxAdjustedPrices = [];
+
+prices.forEach( (price, idx, prices) => {
+  // Create object for each grand total:
+  const grandTotal = {
+    index: idx,
+    basePrice: price,
+    taxAdjustedPrice: price * (1 + salesTax)
+  };
+  taxAdjustedPrices.push(grandTotal);
+});
+
+// console.log(taxAdjustedPrices);
