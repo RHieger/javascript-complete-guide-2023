49da47ce87a2f5371b5409f647cb9d90bcdb960d
Robert Hieger <rhieger@gmail.com>
2022-12-21 23:25:49 -0500

    Lecture 8.194: Build priceObj and push to taxAdjustedPrices[]
    Encapsulate priceObj into displayPriceData()
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.194/app.js
+++ section-8/lecture-08.194/app.js
@@ -17,8 +17,7 @@
 const prices = [10.99, 5.99, 3.99, 6.59];
 
 // Tax Rate
-const salesTax = 0.08875;
-const taxAdjustedPrices = [];
+const tax = 0.08875;
 
 // Log original prices[] array to console.
 console.log('\n\n\tDEMO OF forEach() METHOD:');
@@ -29,14 +28,45 @@ console.table(prices);
 // Log NYC Sales Tax Rate to Console:
 console.log('\n\tNYC Sales Tax Rate:');
 console.log('\t------------------------');
-console.log(`\tSales Tax: ${salesTax}`);
+console.log(`\tSales Tax: ${tax}`);
 
 // Log Base Price, Tax, and Grand Total
 // to console.
 
+// Array to which price taxes and total data
+// will be pushed.
+const taxAdjustedPrices = [];
+
 // Displays base price, sales tax and grand
 // total for each element in the prices array.
-
-const displayGrandTotals = () => {
-  // TBD
+const displayPriceData = () => {
+  prices.forEach( (basePrice) => {
+    const priceObj = {
+      price: basePrice,
+      salesTax: parseFloat((basePrice * tax)
+        .toFixed(2)),
+      grandTotal: parseFloat( (basePrice + tax)
+        .toFixed(2))
+  };
+    taxAdjustedPrices.push(priceObj);
+  });
+  console.log(taxAdjustedPrices);
 };
+
+displayPriceData();
+
+/*
+const displayGrandTotals = (idx, basePrice, tax, total) => {
+  const total = basePrice + salesTax;
+  const priceObject = {
+    index: idx,
+    price: basePrice,
+    salesTax: (basePrice * tax).toFixed(2),
+    grandTotal: total
+  };
+  prices.forEach((idx, basePrice, salesTax, grandTotal) => {
+    taxAdjustedPrices.push(priceObject);
+  });
+  console.log(taxAdjustedPrices);
+};
+*/
