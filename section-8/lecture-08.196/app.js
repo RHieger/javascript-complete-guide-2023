2606a7973053791edf0b10fcdb2f30b006aa66b4
Robert Hieger <rhieger@gmail.com>
2022-12-24 18:36:13 -0500

    Lecture 8.196: Delete Superfluous Code.
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

---
+++ section-8/lecture-08.196/app.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Lecture 8.196: sort()ing and reverse()ing
+ * 
+ * Robert Hieger
+ * 12/24/2022
+ * 
+ * Objective: Demonstrate the sort() and reverse()
+ * methods exposed by the Array object.
+ * 
+ */
+
+const prices = [
+  10.99, 5.99, 3.99, 6.59
+];
