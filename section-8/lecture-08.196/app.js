ff001b3d86075158266a7cd21b4880101603edde
Robert Hieger <rhieger@gmail.com>
2022-12-24 19:34:54 -0500

    Lecture 8.196: Demontrate sort() Method.

--- section-8/lecture-08.196/app.js
+++ section-8/lecture-08.196/app.js
@@ -19,3 +19,28 @@ console.log('\t----------------------------\n');
 
 console.log('\nContents of prices[]:');
 console.table(prices);
+
+console.log('\n\tThe sort() Method:');
+console.log('-----------------\n');
+
+// Sort prices in ascending order.
+// NOTE: By default, sort() will parse the values of
+// an array as a string, which will produce different
+// results than a numeric ascending sort.
+// 
+// A variant of the sort method allows the comparison
+// of two values to see which is greater and reverses
+// order wherever necessary, as seen in the example
+// stored to sortedPrices[].
+const sortedPrices = prices.sort( (a, b) => {
+  if (a > b) {
+    return 1;
+  } else if (a === b) {
+    return 0;
+  } else {
+    return
+  }
+});
+
+console.log('\nprices[] Array Sorted in Ascending Order:');
+console.table(sortedPrices);
