28b97517f120a8dcd0369a8bb35b50ec52f81cb5
Robert Hieger <rhieger@gmail.com>
2022-12-26 11:36:53 -0500

    Lecture 8.202: Delete Superfluous Code
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.202/app.js
+++ section-8/lecture-08.202/app.js
@@ -9,52 +9,9 @@
  * 
  */
 
-const data = 'new york;10.99;2000';
+const nameFragments = [ 'Robert', 'Hieger' ];
 
 // Log Intro and Original Array Contents to Console.
 
-console.log('\n\n\tDEMONSTRATION OF split() AND join():');
+console.log('\n\n\tDEMONSTRATION OF spread(...) Method:');
 console.log('\t------------------------------------\n');
-
-console.log(`\n\tValue of data String: '${data}'\n`);
-
-// Demonstrate the split() Method:
-// NOTE: The split() method is a String method, not
-// an array method.
-
-// It takes an argument of separator between desired
-// elements, in this case, semicolon. There is an
-// optional argument that specifies a limit on the
-// number of array elements returned.
-
-const transformedData = data.split(';');
-
-console.log(
-  '\n\nDemonstration of split() Method (String Function):'
-);
-console.log(
-  '-------------------------------------------------\n'
-);
-
-// Log new transformedData[] to console:
-console.log('\ndata Transformed Into transformedData[]:')
-console.table(transformedData);
-
-// Demonstrate the join() Method:
-
-const person = [ 'Robert', 'Hieger' ];
-
-// NOTE: The join() method is an Array method, unlike its
-// mirror image, the split() method.
-
-console.log('\nDemonstration of the join() Method (Array Function):')
-console.log('--------------------------------------------------\n\n');
-
-console.log('Contents of person[] Array:');
-console.table(person);
-
-const transformedPerson = person.join(' ');
-
-console.log(
-  `\n\tValue of Transformed person[] Array: '${transformedPerson}'\n`
-);
