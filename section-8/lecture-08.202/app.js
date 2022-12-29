73c43c4fa61141b9e5e19dd9479df83f8957dc31
Robert Hieger <rhieger@gmail.com>
2022-12-29 00:05:41 -0500

    Lecture 8.202: Create Deep Clone Using map()
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.202/app.js
+++ section-8/lecture-08.202/app.js
@@ -3,7 +3,7 @@
  * Lecture 8.202: The Spread Operator (...)
  * 
  * Robert Hieger
- * 12/26/2022
+ * 12/28/2022
  * 
  * Objective: TBD.
  * 
@@ -97,6 +97,105 @@ console.log(
 );
 
 console.log(`\nLowest Value in numbers[]: ${Math.min(...numbers)}\n`);
+
+// OBJECTIVE #4: Demonstrate that using the spread operator to copy
+// an array of objects does NOT copy the objects, because objects
+// are reference values.
+
+const people = [
+  { name: 'Sam', age: 60 },
+  { name: 'Jane Doe', age: 32 },
+  { name: 'Joe Schmo', age: 65 }
+];
+
+console.log(
+  '\nUSE CASE #4: Using the Spread Operator to Clone an ' +
+  '\n\t\t\t Array of Objects:'
+);
+
+console.log('\nContents of the people[] Array:\n');
+
+people.forEach( person => console.log(person) );
+
+// Copy people[] array
+const copiedPeople = [...people];
+
+console.log(
+  '\nHere\'s the contents of the new copiedPeople[] Array:\n'
+);
+
+copiedPeople.forEach( copiedPerson => console.log(copiedPerson) );
+
+copiedPeople[1].name = 'Josephine Deerborn';
+copiedPeople[1].age = 45;
+
+console.log('\nLooks okay, right? But get ready for a surprise!\n');
+console.log('\nLet\'s set the value of copiedPeople[1] to a new value.\n')
+console.log('\nHere\'s copiedPeople once again:\n');
+
+copiedPeople.forEach( copiedPerson => console.log(copiedPerson) );
+
+console.log('\nNow let\'s take another look at people[]:\n');
+people.forEach( person => console.log(person) );
+
+// OBJECTIVE #5: Demonstrate Deep Clone Using map()
+
+// Reset people array to original value:
+people[1] = { name: 'Jane Doe', age: 32 };
+
+console.log(
+  '\nNOW WHAT? changing the value of the element ' +
+  'at index 1\nof copiedPeople[] changes the value' +
+  ' in both arrays. We\ndon\'t want that! There is a' +
+  ' way out of this using the\nmap() method.\n\n'
+);
+
+console.log('First, let\'s reset the people[] array.\n\n');
+console.log('Repaired people[] array:\n');
+console.table(people);
+
+const peopleCorrectedCopy = [
+  ...people.map(
+    person => (
+      {
+        name: person.name,
+        age: person.age
+      }
+    )
+  )
+];
+
+console.log('\nContents of new peopleCorrectedCopy[]:\n\n');
+console.table(peopleCorrectedCopy);
+
+console.log(
+  'Now let\'s change the first element in this array!'
+);
+
+peopleCorrectedCopy[0] = {
+  name: 'John Q. Public',
+  age: 72
+};
+
+console.log('\nNow Let\'s see the changed array:\n');
+console.table(peopleCorrectedCopy);
+
+console.log(
+  '\nNow let\'s see if the change affected the ' +
+  'people[] array:\n'
+);
+console.table(people);
+
+console.log(
+  '\nAs you can see, there is no affect' +
+  'on the people[]\narray. Bear in mind ' +
+  'that this alorithm is not foolproof.\nIf ' +
+  'even one element of the original array ' +
+  'has an\nadditional property in its object ' +
+  'it will be necessary\nto make yet more ' +
+  'adjustments to make certain that a deep' +
+  '\nclone is returned.\n'
+);
 
 // Instructional Message:
 console.log(
