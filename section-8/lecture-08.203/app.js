1c22c6ff9e9efb886f618a1a0d283561921409bf
Robert Hieger <rhieger@gmail.com>
2022-12-30 17:27:16 -0500

    Lecture 8.103: Refine output of destructure array
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-8/lecture-08.203/app.js
+++ section-8/lecture-08.203/app.js
@@ -10,4 +10,44 @@
 
 const nameFragments = [ 'Sam', 'Rogers', 'Mr.', 42  ];
 
+console.log('\n\n\tDEMONSTRATE USE OF ARRAY DESTRUCTURING:');
+console.log('\t--------------------------------------\n');
+
+console.log('\nWe start with the nameFragments[] array:');
 console.table(nameFragments);
+
+console.log(
+  '\n\nNow we will destructure the above array' +
+  ' breaking it into\nits constituent elements to' +
+  ' create a string object, using\nthe following' +
+  ' syntax:\n\n' +
+  'const [firstName, lastName, ...additionalData] ' 
+  + '= nameFragments;\n\n'
+);
+
+// Destructure the array:
+const [
+  firstName, lastName,
+  ...additionalData
+] = nameFragments;
+
+console.log(
+  '\nWhat the above statement does is to' +
+  ' break apart the above\narray into strings' +
+  ' that can then be assembled however we\nsee' +
+  ' fit. "...additionalData" uses the rest ' +
+  'operator to\nextract all remaining data ' +
+  'from the array. Note, however,\nthat because ' +
+  'the rest of the data is two array elements,' +
+  '\nthey will not be transformed into a string.'
+);
+
+console.log(
+  '\n\nHere is the output of the destructured array:\n'
+);
+
+// Output destructured array:
+
+console.log(firstName, lastName);
+console.log('\nAdditional Data:\n');
+console.table(additionalData);
