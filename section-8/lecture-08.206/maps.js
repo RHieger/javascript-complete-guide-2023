6181bfd67bdb4c956048ebcb9f61620f42935f09
Robert Hieger <rhieger@gmail.com>
2022-12-31 11:49:41 -0500

    Lecture 8.206: Declare Two Objects for Map()
    person1() and person2() are objects to be used with the map()
    function in order to append separately stored ancillary data
    without bloating the information stored in each of the
    objects--currently { name: 'name'  }.

--- section-8/lecture-08.206/maps.js
+++ section-8/lecture-08.206/maps.js
@@ -11,3 +11,10 @@
 
 console.log('\n\n\tDEMONSTRATE MAPS:');
 console.log('\t----------------\n\n');
+
+// 2 objects created for use with Map() Object
+const person1 = { name: 'Joanie' };
+const person2 = { name: 'Bobby' };
+
+console.log('Created two objects--person1 and person2:\n');
+console.log(person1, person2);
