fa7e36643485c95eb0e533e0f6fa6090ab87b091
Robert Hieger <rhieger@gmail.com>
2022-12-31 20:46:31 -0500

    Lecture 8.106: Refine Output to Console
    1) Add informational messages prior to each objective demonstrated.
    2) Introduce each algorithm employed when looping through the member
    entries of the Map() object.

--- section-8/lecture-08.206/maps.js
+++ section-8/lecture-08.206/maps.js
@@ -19,6 +19,14 @@ const person2 = { name: 'Bobby' };
 console.log('Created two objects--person1 and person2:\n');
 console.log(person1, person2);
 
+// OBJECTIVE #1: Create and log to console personData() map
+// to link ancillary data to the objects declared above.
+
+console.log(
+  '\nOBJECTIVE #1: Add data to person1() and person2()\n' +
+  'Objects by Creating personData() Map:\n'
+);
+
 const personData = new Map(
   [
     [
@@ -30,11 +38,6 @@ const personData = new Map(
   ]  
 );
 
-console.log(personData);
-
-// Use the get() method:
-console.log( personData.get(person1) );
-
 // Add person2 to personData:
 personData.set(
   person2,
@@ -45,23 +48,66 @@ personData.set(
 
 console.log(personData);
 
-// 4 Algorithms for looping through information in Map:
-// Method #1 Using entries() Method
+// OBJECTIVE #2: Use the get() method to choose
+// an entry value by key.
+
+console.log(
+  '\nOBJECTIVE #2: Extract Entry Value by Key\n' +
+  'Using get() Method:\n\n'
+);
+
+console.log('Extracting value of person1 key...\n\n');
+console.log( personData.get(person1) );
+
+// OBJECTIVE #3: Demonstrate 4 Algorithms for Extracting
+// different pieces of data from personData() Map:
+
+console.log(
+  '\nOBJECTIVE #3: Use 4 Algorithms to Extract Specified\n' +
+  'Data from personData() Map.\n\n' 
+);
+console.log(
+  'Algorithm #1: Log Complete Entries Using\n' +
+  'entries() Method...\n\n'
+);
+
+// Algorithm #1: Using entries() Method--logs complete Map() objects
 for ( const entry of personData.entries() ) {
   console.log(entry);
 };
 
-// Method #2 Using Array Destructuring
+// Algorithm #2: Using Array Destructuring--also logs
+// complete objects
+
+console.log(
+  '\n\nAlgorithm #2: Log Complete Entries Using\n' +
+  'Array Destructuring with entries() Method:\n\n'
+);
+
 for ( const [key, value] of personData.entries() ) {
   console.log(key, value);
 }
 
-// Method #3 Using the keys() Method
+// Algorithm #3: Using the keys() Method--logs only keys
+// from the objects
+
+console.log(
+  '\n\nAlgorithm #3: Log only Keys of Entries ' +
+  'Using the keys() Method:\n\n'
+);
+
 for ( const key of personData.keys() ) {
   console.log(key);
 };
 
-// Method #4 Using the values() Method
+// Algorithm #4: Using the values() Method--logs only the
+// values from the objects.
+
+console.log(
+  '\n\nAlgorithm #4: Log only Values of Entries ' +
+  'Using the values() Method:\n\n'
+);
+
 for ( const value of personData.values() ) {
   console.log(value);
 };
