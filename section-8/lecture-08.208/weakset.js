c9d8c1a0d5e20218af5d63619049cc359fdfbf1b
Robert Hieger <rhieger@gmail.com>
2023-01-01 11:06:59 -0500

    Complete Lecture 8.208: Finish Embedded Documentation/Comments

--- section-8/lecture-08.208/weakset.js
+++ section-8/lecture-08.208/weakset.js
@@ -5,12 +5,26 @@
  * Robert Hieger
  * January 1, 2023
  * 
- * OBJECTIVE: TBD.
+ * OBJECTIVE: Explore the utility of the Weakset.
+ * 
+ * Weaksets are so named because of their ability to
+ * release objects for garbage collection without a
+ * developer having to use the delete() method.
+ * 
+ * They save the developer from having to remember to
+ * set the object to null or use the delete() method.
+ * 
+ * As garbage collection is an automatic feature of the
+ * JavaScript engine, the developer has no way of knowing
+ * when the garbage is collected, but Weakset assures
+ * that it will be collected.
  * 
  */
 
+
 // Object for use with WeakSet:
-// NOTE: Sets are also capable of containing objects,
+
+// NOTE: Weaksets are also capable of containing objects,
 // not just strings.
 let person = { name: 'Bobby' };
 
