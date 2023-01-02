3d91b5237a4291e0e640d143183f9cc1d2e83bb0
Robert Hieger <rhieger@gmail.com>
2023-01-02 18:33:11 -0500

    Assignment #6: Create Number Array and Log to Console

--- section-8/assignment-6/assignment.js
+++ section-8/assignment-6/assignment.js
@@ -5,6 +5,22 @@
  * Robert Hieger
  * January 2, 2023
  * 
- * OBJECTIVE: TBD.
+ * OBJECTIVES: Review Arrays and Iterables.
+ * 
+ * Task #1 Array Manipulation
+ * 
+ * 1) Create an array of numbers. Log to console.
  * 
  */
+
+console.log('\n\n\tREVIEW ARRAYS & ITERABLES');
+console.log('\t-------------------------\n\n');
+
+const myNumbers = [
+  1, 2, 3, 5, 35, 78, 128, 256, 512, 1024
+];
+
+console.log('\n\nTask #1: Create Array of Numbers:\n\n');
+console.log('Operation #1: Log myNumbers[] to console:\n\n');
+console.log('Contents of myNumbers[]:\n');
+console.table(myNumbers);
