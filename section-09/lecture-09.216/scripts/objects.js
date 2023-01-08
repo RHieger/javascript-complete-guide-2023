fdb29fe61cde689b6e6b01da28d4f4764f47ea6f
Robert Hieger <rhieger@gmail.com>
2023-01-07 19:19:28 -0500

    Lecture 9.216: Migrated Code from Lecture 9.215

---
+++ section-09/lecture-09.216/scripts/objects.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * Lecture 9.215: Objects Recap
+ * 
+ * Robert Hieger
+ * January 7, 2023
+ * 
+ * OBJECTIVE: Demonstrate creation of an object,
+ * including a method to display alert box with
+ * message 'Hi, there!'
+ * 
+ * Demonstrate how to access properties and/or
+ * methods in the Object.
+ * 
+ */
+
+const person = {
+  name: 'Bart',
+  age: 45,
+  hobbies: [
+    'astronomy',
+    'hiking'
+  ],
+  greet: function() {
+    alert('Hi, there!');
+  }
+};
+
+// Greet user:
+
+person.greet();
