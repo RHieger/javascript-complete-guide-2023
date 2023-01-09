ac255599d1c166188022a0b61c8fb7264c7e4776
Robert Hieger <rhieger@gmail.com>
2023-01-09 11:10:07 -0500

    Lecture 9.220: Capture Nodes for "Add Movie" and "Search"

--- section-09/lecture-09.220/scripts/objects.js
+++ section-09/lecture-09.220/scripts/objects.js
@@ -5,6 +5,19 @@
  * Robert Hieger
  * January 9, 2023
  * 
- * OBJECTIVE: TBD.
+ * OBJECTIVE: Create a demo application that allows a
+ * user to enter the name and additional information
+ * about a favorite movie. Additionally, search
+ * functionality will be included to enable a user to
+ * search for a movie (ostensibly already entered).
+ * 
+ * TASK #1: Capture the nodes necessary to reference
+ * the two buttons--Add Movie and Search.
  *  
  */
+
+// Capture button nodes:
+const addMovieBtn = document
+  .getElementById('add-movie-btn');
+
+const searchBtn = document.getElementById('search-btn');
