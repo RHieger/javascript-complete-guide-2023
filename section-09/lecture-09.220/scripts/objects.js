743fd5bdca810b27f474b01bd715f5cd93740f0b
Robert Hieger <rhieger@gmail.com>
2023-01-09 13:19:16 -0500

    Complete Lecture 9.220: Code addMovieHandler()

--- section-09/lecture-09.220/scripts/objects.js
+++ section-09/lecture-09.220/scripts/objects.js
@@ -11,8 +11,12 @@
  * functionality will be included to enable a user to
  * search for a movie (ostensibly already entered).
  * 
- * TASK #1: Capture the nodes necessary to reference
+ * Task #1: Capture the nodes necessary to reference
  * the two buttons--Add Movie and Search.
+ * 
+ * Task #2: Code the addMovieHandler() function.
+ * 
+ * Task #3: Add an event listener to "Add Movie" Button.
  *  
  */
 
@@ -21,3 +25,42 @@ const addMovieBtn = document
   .getElementById('add-movie-btn');
 
 const searchBtn = document.getElementById('search-btn');
+
+// Datastore for Movie Objects:
+const movies = [];
+
+// Movie Functions:
+
+const addMovieHandler = () => {
+  
+  // Capture required nodes:
+  const title = document.getElementById('title').value;
+  const extraName = document
+    .getElementById('extra-name').value;
+  const extraValue = document
+    .getElementById('extra-value').value;
+  
+  // Validate user input:
+  if (
+      title.trim() === '' ||
+      extraName.trim() === '' ||
+      extraValue.trim() === ''
+    ) {
+      return;
+  }
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    // Assures that ID number is integer minimum of 1000.
+    id: Math.trunc(Math.random() * 1000) + 1000
+  };
+  movies.push(newMovie);
+  // FOR DIAGNOSTIC PURPOSES ONLY:
+  console.log(movies);
+};
+
+// EVENT LISTENERS:
+
+addMovieBtn.addEventListener('click', addMovieHandler);
