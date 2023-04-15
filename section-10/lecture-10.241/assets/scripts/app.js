9dd3a004c2606aade3956591ece3a9c3d37fad17
Robert Hieger <rhieger@gmail.com>
2023-04-15 12:25:46 -0400

    Lecture 10.241: Refactor products Object
    1. Move products object into productList Object.
    
    2. Conver products object to make it a property of productList.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-10/lecture-10.241/assets/scripts/app.js
+++ section-10/lecture-10.241/assets/scripts/app.js
@@ -13,22 +13,25 @@
  *  1. Create a dummy array of two product objects containing titles,
  *  prices and an image url for product photo.
  *
+ *  2. Create stub for productList object.
+ *
+ *  3. Refactor products Object, moving it into productList and making
+ *  the object into a property of productList.
+ *
 */
 
-// Array of two product objects
-const products = [
-  {
-    title: 'A Pillow', imageUrl: 'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
-    price: 19.99,
-    description: 'A soft pillow!'
-  },
-  {
-    title: 'A Carpet', imageUrl: 'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
-    price: 89.99,
-    description: 'A carpet which you might like - or not.'
-  }
-];
-
 const = productList {
-  // TBD 
+  
+  products:[
+    {
+      title: 'A Pillow', imageUrl: 'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
+      price: 19.99,
+      description: 'A soft pillow!'
+    },
+    {
+      title: 'A Carpet', imageUrl: 'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
+      price: 89.99,
+      description: 'A carpet which you might like - or not.'
+    }
+  ] 
 };
