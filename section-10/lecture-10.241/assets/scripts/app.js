5733baa6f9a349d34ee53679f8683cd779a51dbd
Robert Hieger <bobby@Roberts-MBP-2.fios-router.home>
2023-04-15 11:36:23 -0400

    Lecture 10.241: Add Nominal Embedded Documentation/Comments
    Signed-off-by: Robert Hieger <bobby@Roberts-MBP-2.fios-router.home>

---
+++ section-10/lecture-10.241/assets/scripts/app.js
@@ -0,0 +1,13 @@
+/**
+ *
+ *  Lecture 10.241: Getting Started with OOP Code
+ *
+ *  This lecture introduces the concepts behind object-oriented
+ *  programming (OOP) and introduces the project for this module.
+ *  This module builds from the ground up a front-end for an
+ *  online store with product lists, individual products for sale
+ *  and the functionality to allow a user to order product(s).
+ *
+ *  Tasks for this lecture: TBD
+ *
+*/
