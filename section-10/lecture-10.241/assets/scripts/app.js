a0ba60c291477977193ee60cdfc58d81d46c25b0
Robert Hieger <bobby@Roberts-MBP-2.fios-router.home>
2023-04-15 12:00:15 -0400

    Lecture 10.241: Create Array with 2 Dummy Product Objects
    Signed-off-by: Robert Hieger <bobby@Roberts-MBP-2.fios-router.home>

--- section-10/lecture-10.241/assets/scripts/app.js
+++ section-10/lecture-10.241/assets/scripts/app.js
@@ -8,6 +8,23 @@
  *  online store with product lists, individual products for sale
  *  and the functionality to allow a user to order product(s).
  *
- *  Tasks for this lecture: TBD
+ *  Tasks for this lecture:
+ *
+ *  1. Create a dummy array of two product objects containing titles,
+ *  prices and an image url for product photo.
  *
 */
+
+// Array of two product objects
+const products = [
+  {
+    title: 'A Pillow', imageUrl: 'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
+    price: 19.99,
+    description: 'A soft pillow!'
+  },
+  {
+    title: 'A Carpet', imageUrl: 'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
+    price: 89.99,
+    description: 'A carpet which you might like - or not.'
+  }
+];
