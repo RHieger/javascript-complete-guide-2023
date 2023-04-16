c7029a1a9e92f90623a0b9bf50dedc63d8a1d5b8
Robert Hieger <rhieger@gmail.com>
2023-04-16 13:56:46 -0400

    Lecture 10.243: Refactor productList()
    1. Make call to constructor method of Product Class to instantiate products.
    2. Move pre-existing product properties as arguments for the Product.constructor() method.
    
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-10/lecture-10.243/assets/scripts/app.js
+++ section-10/lecture-10.243/assets/scripts/app.js
@@ -14,47 +14,49 @@
 */
 
 class Product {
-  
+
   constructor(title, image, desc, price) {
     this.title = title;
     this.imageUrl = image;
     this.description = desc;
     this.price = price;
   }
-    
+
 }
 
 const productList = {
 
-  products:[
-    {
-      title: 'A Pillow', imageUrl: 'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
-      price: 19.99,
-      description: 'A soft pillow!'
-    },
-    {
-      title: 'A Carpet', imageUrl: 'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
-      price: 89.99,
-      description: 'A carpet which you might like - or not.'
-    }
+  products: [
+  	new Product(
+  	  'A Pillow',
+  	  'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
+  	  'A soft pillow!',
+  	  19.99
+  	),
+  	new Product(
+  	  'A Carpet',
+  	  'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
+	  'A carpet which you might like - or not.',
+	  89.99
+  	)
   ],
   render() {
-    
+
     // Capture node for root element of product list
     const renderHook = document.getElementById('app');
-    
+
     // productList node
     const prodList = document.createElement('ul');
-    
+
     // Add styling
     prodList.className = 'product-list';
-    
+
     // Assemble List of products:
     for (const prod of this.products) {
-    
+
       const prodEl = document.createElement('li');
       prodEl.className = 'product-item';
-      
+
       // Set content for prodEl:
       prodEl.innerHTML = `
         <div>
