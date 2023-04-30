8c37fe3d7bfb0cbffaf0f0caf97c81340e4a3acb
Robert Hieger <rhieger@gmail.com>
2023-04-30 12:23:25 -0400

    Lecture 10.247: Migrate Code from Lecture 10.246
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

---
+++ section-10/lecture-10.247/assets/scripts/app.js
@@ -0,0 +1,117 @@
+/**
+ *
+ *  Lecture 10.246: Building Class Methods & Working with "this"
+ *  Robert Hieger
+ *  04/30/2023
+ *
+ *  This lecture introduces the concept of class methods and the
+ *  convoluted logic behind the "this" keyword.
+ *
+ *  Tasks for this lecture: TBD
+ *
+ *  1. Within ProductItem.render(), capture <button> within
+ *     productEl node.
+ *
+ *  2. Add 'click' event listener to addCartButton node.
+ *
+ *  3. Create stub for addToCart() method and link to event listener
+ *     as callback.
+ *
+*/
+
+class Product {
+
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+
+}
+
+class ProductItem {
+
+  constructor(product) {
+  	this.product = product;
+  }
+
+  addToCart() {
+  	// FOR DIAGNOSTIC PURPOSES ONLY
+  	console.log('Adding product to cart...');
+  	console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+
+    // Set content for prodEl:
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+          <div class="product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>\$${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `
+    const addCartButton = prodEl.querySelector('button');
+    addCartButton.addEventListener('click', this.addToCart.bind(this));
+    return prodEl;
+  }
+
+}
+
+class ProductList {
+
+  products = [
+  	new Product(
+  	  'A Pillow',
+  	  'http://www.clker.com/cliparts/b/0/b/d/13444048401912257549buckwheat-pillow-hi.png',
+  	  'A soft pillow!',
+  	  19.99
+  	),
+  	new Product(
+  	  'A Carpet',
+  	  'https://secure.img1-fg.wfcdn.com/im/47722856/resize-h800-w800%5Ecompr-r85/1317/131735735/Serapi+Hand-Knotted+Oriental+Area+Rug+in+Rust%2FIvory%2FNavy.jpg',
+	  'A carpet which you might like - or not.',
+	  89.99
+  	)
+  ];
+
+  // EXPERIMENTAL CODE
+  constructor() {
+  	// TBD
+  }
+
+  render() {
+
+  	// Capture node for root element of product list
+    const renderHook = document.getElementById('app');
+
+    // productList node
+    const prodList = document.createElement('ul');
+
+    // Add styling
+    prodList.className = 'product-list';
+
+    // Assemble List of products:
+    for (const prod of this.products) {
+
+	  const productItem = new ProductItem(prod);
+	  const prodEl = productItem.render();
+
+      // Add product to product list.
+      prodList.append(prodEl);
+
+    }
+	renderHook.append(prodList);
+  }
+
+}
+
+const productList = new ProductList();
+productList.render();
