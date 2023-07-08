5201d62a6b06c355a4a07ac1d03405d1b3b56547
Robert Hieger <rhieger@gmail.com>
2023-07-07 21:12:30 -0400

    Lecture 10.256: Add nominal embedded documentation/comments
    Signed-off-by: Robert Hieger <rhieger@gmail.com>

--- section-10/lecture-10.256/assets/scripts/app.js
+++ section-10/lecture-10.256/assets/scripts/app.js
@@ -1,24 +1,13 @@
 /**
  *
- *  Lecture 10.255: Overriding Methods and the super() Constructor
+ *  Lecture 10.256: super() Constructor Execution, Order & "this"
  *  Robert Hieger
- *  06/17/2023
+ *  07/07/2023
  *
- *  Now that inheritance has been used for all remaining classes
- *	in which it made sense to do so, this lecture turns its attention
- *	to overriding methods and use of the super() constructor.
+ *  This lecture explores the importance of positioning of the super() function
+ *  and how it can make or break code.
  *
- *  Tasks for this lecture:
- *
- *	1. Address redundancy of calls to render() method in Shop class by
- *		 removing calls to the method within this class.
- *
- *	2. Address redundancy of calls to render() method in ProductList class
- *		 by removing calls to the method within this class.
- *
- *	3. Add constructor to Shop class calling this.render() to render the
- *		 base element for the application. Throws error this.products is
- *		 not iterable.
+ *	Tasks Completed: TBA
  *
 */
 
